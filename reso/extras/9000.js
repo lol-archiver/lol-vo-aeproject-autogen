a0d679865fdd9ac16e08d186ac6b7410c9e72399
DanoR <zheung@vip.qq.com>
2020-03-19 09:50:19 +0800

    9000

---
+++ reso/extras/9000.js
@@ -0,0 +1,125 @@
+module.exports = {
+	event: {
+		FirstEncounterAnnie: {
+			target: '${C.path.project.autogen}reso/icons/Annie/0.png'
+		},
+		FirstEncounterEvelynn: {
+			target: '${C.path.project.autogen}reso/icons/Evelynn/0.png'
+		},
+		FirstEncounterGaren: {
+			target: '${C.path.project.autogen}reso/icons/Garen/0.png'
+		},
+		FirstEncounterJinx: {
+			target: '${C.path.project.autogen}reso/icons/Jinx/0.png'
+		},
+		FirstEncounterKindred: {
+			target: '${C.path.project.autogen}reso/icons/Kindred/0.png'
+		},
+		FirstEncounterLux: {
+			target: '${C.path.project.autogen}reso/icons/Lux/0.png'
+		},
+		FirstEncounterMissFortune: {
+			target: '${C.path.project.autogen}reso/icons/MissFortune/0.png'
+		},
+		FirstEncounterNocturne: {
+			target: '${C.path.project.autogen}reso/icons/Nocturne/0.png'
+		},
+		FirstEncounterNunu: {
+			target: '${C.path.project.autogen}reso/icons/Nunu/0.png'
+		},
+		FirstEncounterRiven: {
+			target: '${C.path.project.autogen}reso/icons/Riven/0.png'
+		},
+		FirstEncounterSoraka: {
+			target: '${C.path.project.autogen}reso/icons/Soraka/0.png'
+		},
+		FirstEncounterSwain: {
+			target: '${C.path.project.autogen}reso/icons/Swain/0.png'
+		},
+		FirstEncounterSylas: {
+			target: '${C.path.project.autogen}reso/icons/Sylas/0.png'
+		},
+		FirstEncounterTahmKench: {
+			target: '${C.path.project.autogen}reso/icons/TahmKench/0.png'
+		},
+		FirstEncounterVayne: {
+			target: '${C.path.project.autogen}reso/icons/Vayne/0.png'
+		},
+		FirstEncounterYasuo: {
+			target: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+		},
+		FirstEncounterDemacia: {
+			target: '${C.path.project.autogen}reso/icons/regions/Demacia.png'
+		},
+
+		KillChampionAnnie: {
+			target: '${C.path.project.autogen}reso/icons/Annie/0.png'
+		},
+		KillChampionEvelynn: {
+			target: '${C.path.project.autogen}reso/icons/Evelynn/0.png'
+		},
+		KillChampionGaren: {
+			target: '${C.path.project.autogen}reso/icons/Garen/0.png'
+		},
+		KillChampionJinx: {
+			target: '${C.path.project.autogen}reso/icons/Jinx/0.png'
+		},
+		KillChampionKindred: {
+			target: '${C.path.project.autogen}reso/icons/Kindred/0.png'
+		},
+		KillChampionLux: {
+			target: '${C.path.project.autogen}reso/icons/Lux/0.png'
+		},
+		KillChampionMissFortune: {
+			target: '${C.path.project.autogen}reso/icons/MissFortune/0.png'
+		},
+		KillChampionNocturne: {
+			target: '${C.path.project.autogen}reso/icons/Nocturne/0.png'
+		},
+		KillChampionNunu: {
+			target: '${C.path.project.autogen}reso/icons/Nunu/0.png'
+		},
+		KillChampionRiven: {
+			target: '${C.path.project.autogen}reso/icons/Riven/0.png'
+		},
+		KillChampionSoraka: {
+			target: '${C.path.project.autogen}reso/icons/Soraka/0.png'
+		},
+		KillChampionSwain: {
+			target: '${C.path.project.autogen}reso/icons/Swain/0.png'
+		},
+		KillChampionSylas: {
+			target: '${C.path.project.autogen}reso/icons/Sylas/0.png'
+		},
+		KillChampionTahmKench: {
+			target: '${C.path.project.autogen}reso/icons/TahmKench/0.png'
+		},
+		KillChampionVayne: {
+			target: '${C.path.project.autogen}reso/icons/Vayne/0.png'
+		},
+		KillChampionYasuo: {
+			target: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+		},
+
+		FiddlesticksScarecrowEffigy: {
+			target: '${C.path.project.autogen}reso/icons/Fiddlesticks/P.png'
+		},
+		FiddlesticksE: {
+			target: '${C.path.project.autogen}reso/icons/Fiddlesticks/E.png'
+		},
+		FiddlesticksR: {
+			target: '${C.path.project.autogen}reso/icons/Fiddlesticks/R.png'
+		},
+	},
+	lines: {
+		'1C3C3831': [
+			{
+				oper: 'after',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Riven/1.png',
+				line: 's(・｀ヘ´・;)ゞ'
+			}
+		],
+	}
+};
