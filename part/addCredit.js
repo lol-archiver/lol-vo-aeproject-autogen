c2924e65b85781865f0bedc1d552f55c25dc2eeb
DanoR <zheung@vip.qq.com>
2020-07-13 09:22:06 +0800

    200713

--- part/addCredit.js
+++ part/addCredit.js
@@ -1,4 +1,7 @@
 P.addCredit = function addCredit() {
+	var textThankyou = '感谢观看！\n';
+	var textInfo = '解包/听写/整理/制作\n    by DanoR(Nuogz)\n\n' + C.emote + '\n\n如有错漏，敬请留言';
+
 	var splashEnd = F(C.path.splash);
 	var layerSplashEnd = T.compMain.layers.add(splashEnd, D.full);
 	layerSplashEnd.startTime = D.linesEnd + 2;
@@ -7,7 +10,7 @@ P.addCredit = function addCredit() {
 	layerSplashEnd.transform.opacity.setValueAtTime(D.linesEnd + 2, 0);
 	layerSplashEnd.transform.opacity.setValueAtTime(D.linesEnd + 3.4, 100);
 
-	var layerThankyou = T.compMain.layers.addText('感谢观看！\n');
+	var layerThankyou = T.compMain.layers.addText(textThankyou);
 
 	layerThankyou.startTime = D.linesEnd + 3.4;
 	layerThankyou.duration = D.credit;
@@ -27,7 +30,27 @@ P.addCredit = function addCredit() {
 
 	layerThankyou.sourceText.setValue(textDocThankyou);
 
-	var layerInfo = T.compMain.layers.addText('解包/听写/整理/制作\n    by DanoR(Nuogz)\n感谢道哥的协力校对\n\n' + C.emote + '\n\n如有错漏，敬请留言');
+	var layerThankyou2 = T.compMain.layers.addText(textThankyou);
+
+	layerThankyou2.startTime = D.linesEnd + 3.4;
+	layerThankyou2.duration = D.credit;
+
+	layerThankyou2.transform.position.setValue([44, 154]);
+	layerThankyou2.transform.opacity.setValueAtTime(D.linesEnd + 3.4, 0);
+	layerThankyou2.transform.opacity.setValueAtTime(D.linesEnd + 4.5, 100);
+
+	var textDocThankyou2 = layerThankyou2.sourceText.value;
+	textDocThankyou2.resetCharStyle();
+	textDocThankyou2.fontSize = 100;
+	textDocThankyou2.fillColor = T.rgb(4, 4, 4);
+	textDocThankyou2.font = 'Source Han Mono';
+	textDocThankyou2.applyStroke = true;
+	textDocThankyou2.strokeColor = T.rgb(4, 4, 4);
+	textDocThankyou2.strokeWidth = 4;
+
+	layerThankyou2.sourceText.setValue(textDocThankyou2);
+
+	var layerInfo = T.compMain.layers.addText(textInfo);
 
 	layerInfo.startTime = D.linesEnd + 3.4;
 	layerInfo.duration = D.credit;
@@ -46,4 +69,24 @@ P.addCredit = function addCredit() {
 	textDocInfo.strokeWidth = 1;
 
 	layerInfo.sourceText.setValue(textDocInfo);
+
+	var layerInfo2 = T.compMain.layers.addText(textInfo);
+
+	layerInfo2.startTime = D.linesEnd + 3.4;
+	layerInfo2.duration = D.credit;
+
+	layerInfo2.transform.position.setValue([44, 304]);
+	layerInfo2.transform.opacity.setValueAtTime(D.linesEnd + 3.4, 0);
+	layerInfo2.transform.opacity.setValueAtTime(D.linesEnd + 4.5, 100);
+
+	var textDocInfo2 = layerInfo2.sourceText.value;
+	textDocInfo2.resetCharStyle();
+	textDocInfo2.fontSize = 50;
+	textDocInfo2.fillColor = T.rgb(0, 0, 0);
+	textDocInfo2.font = 'Source Han Mono';
+	textDocInfo2.applyStroke = true;
+	textDocInfo2.strokeColor = T.rgb(0, 0, 0);
+	textDocInfo2.strokeWidth = 1;
+
+	layerInfo2.sourceText.setValue(textDocInfo2);
 };
