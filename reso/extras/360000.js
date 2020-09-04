b15a5341dd99ea882853ffa97e9e95938c7915c2
DanoR <zheung@vip.qq.com>
2020-09-04 13:58:24 +0800

    Samira Extra

---
+++ reso/extras/360000.js
@@ -0,0 +1,85 @@
+module.exports = {
+	emote: '…(⊙＿⊙；)…',
+	event: {
+		'[初遇:英雄:阿木木]':
+			{ target: '${C.path.project.autogen}reso/icons/Amumu/0.png' },
+		'[击杀:英雄:阿木木]':
+			{ target: '${C.path.project.autogen}reso/icons/Amumu/0.png' },
+		'[初遇:英雄:阿兹尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Azir/0.png' },
+		'[击杀:英雄:阿兹尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Azir/0.png' },
+		'[初遇:英雄:德莱文]':
+			{ target: '${C.path.project.autogen}reso/icons/Draven/0.png' },
+		'[击杀:英雄:德莱文]':
+			{ target: '${C.path.project.autogen}reso/icons/Draven/0.png' },
+		'[初遇:英雄:伊泽瑞尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Ezreal/0.png' },
+		'[击杀:英雄:伊泽瑞尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Ezreal/0.png' },
+		'[初遇:英雄:乐芙兰]':
+			{ target: '${C.path.project.autogen}reso/icons/LeBlanc/0.png' },
+		'[击杀:英雄:乐芙兰]':
+			{ target: '${C.path.project.autogen}reso/icons/LeBlanc/0.png' },
+		'[初遇:英雄:厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icons/MissFortune/0.png' },
+		'[击杀:英雄:厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icons/MissFortune/0.png' },
+		'[初遇:英雄:拉莫斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Rammus/0.png' },
+		'[击杀:英雄:拉莫斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Rammus/0.png' },
+		'[初遇:英雄:希维尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Sivir/0.png' },
+		'[击杀:英雄:希维尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Sivir/0.png' },
+		'[初遇:英雄:斯维因]':
+			{ target: '${C.path.project.autogen}reso/icons/Swain/0.png' },
+		'[击杀:英雄:斯维因]':
+			{ target: '${C.path.project.autogen}reso/icons/Swain/0.png' },
+		'[初遇:英雄:锤石]':
+			{ target: '${C.path.project.autogen}reso/icons/Thresh/0.png' },
+		'[击杀:英雄:锤石]':
+			{ target: '${C.path.project.autogen}reso/icons/Thresh/0.png' },
+		'[初遇:英雄:弗拉基米尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Vladimir/0.png' },
+		'[击杀:英雄:弗拉基米尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Vladimir/0.png' },
+		'[初遇:英雄:泽拉斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Xerath/0.png' },
+		'[击杀:英雄:泽拉斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Xerath/0.png' },
+
+
+		'[使用:Q技能:交火:远程射击]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/Q.png' },
+		'[使用:Q技能:交火:近战斩击]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/Q.png' },
+		'[使用:W技能:锋旋]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/W.png' },
+		'[使用:E技能:狂飙]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/E.png' },
+		'[使用:R技能:炼狱扳机:可以使用]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/R_8.png' },
+		'[使用:R技能:炼狱扳机]':
+			{ skill: '${C.path.project.autogen}reso/icons/Samira/R_8.png' },
+	},
+	lines: {
+		'2CA02935': [
+			{
+				oper: 'before',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Amumu/0.png',
+				line: 'ヾ(Ő∀Ő๑)ﾉ',
+			},
+			{
+				oper: 'after',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Amumu/0.png',
+				line: '(╥﹏╥)',
+			},
+		]
+	}
+};
---
+++ reso/extras/360001.js
@@ -0,0 +1,5 @@
+module.exports = {
+	emote: '…(⊙＿⊙；)…',
+	event: {},
+	lines: {}
+};
