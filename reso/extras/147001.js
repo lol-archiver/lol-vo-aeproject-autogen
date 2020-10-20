c50b2c4fa52ffa60fb2a51cc3ed92977c17f9337
DanoR <zheung@vip.qq.com>
2020-10-20 11:53:04 +0800

    147001

---
+++ reso/extras/147001.js
@@ -0,0 +1,120 @@
+module.exports = {
+	emote: '（╯‵□′）╯︵┴─┴',
+	event: {
+		'[击杀:英雄:萨勒芬妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Seraphine/0.png' },
+		'[初遇:英雄:卡兹克]':
+			{ target: '${C.path.project.autogen}reso/icons/Khazix/0.png' },
+		'[初遇:皮肤:DJ 娑娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Sona/0.png' },
+		'[击杀:皮肤:DJ 娑娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Sona/0.png' },
+		'[初遇:皮肤:KDA ALL OUT 阿狸:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Ahri/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[击杀:皮肤:KDA ALL OUT 阿狸:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Ahri/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[初遇:皮肤:KDA ALL OUT 阿卡丽:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Akali/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[击杀:皮肤:KDA ALL OUT 阿卡丽:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Akali/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[初遇:皮肤:KDA ALL OUT 伊芙琳:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Evelynn/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[击杀:皮肤:KDA ALL OUT 伊芙琳:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Evelynn/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[初遇:皮肤:KDA ALL OUT 卡莎:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Kaisa/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[击杀:皮肤:KDA ALL OUT 卡莎:注释:超级巨星形态]':
+			{ target: '${C.path.project.autogen}reso/icons/Kaisa/0.png', head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+
+		'[初遇:系列:KDA:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[击杀:系列:KDA:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[移动:初次:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[移动:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[移动:长途:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+		'[回城:注释:超级巨星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/3.png' },
+
+		'[初遇:系列:KDA:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[击杀:系列:KDA:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[移动:初次:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[移动:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[移动:长途:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[回城:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[攻击:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+		'[击杀:英雄:注释:独立音乐人形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/1.png' },
+
+		'[初遇:系列:KDA:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[击杀:系列:KDA:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[移动:初次:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[移动:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[移动:长途:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[回城:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[攻击:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+		'[击杀:英雄:注释:明日新星形态]':
+			{ head: '${C.path.project.autogen}reso/icons/Seraphine/2.png' },
+
+		'[使用:Q技能:清籁穿云]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/Q.png' },
+		'[使用:Q技能:清籁穿云:回响]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/Q_3.png' },
+		'[使用:W技能:聚合心声]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/W.png' },
+		'[使用:W技能:聚合心声:回响]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/W_3.png' },
+		'[使用:E技能:增幅节拍]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/E.png' },
+		'[使用:E技能:增幅节拍:回响]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/E_3.png' },
+		'[使用:R技能:炫音返场]':
+			{ skill: '${C.path.project.autogen}reso/icons/Seraphine/R.png' },
+	},
+	lines: {
+		'B7BC600A': [
+			{
+				oper: 'after',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Seraphine/3.png${C.path.project.autogen}reso/icons/Khazix/0.png',
+				line: '(〃＞皿＜)',
+			},
+		],
+		'943CED0B': [
+			{
+				oper: 'extra',
+				mark: '注：这语音应是击杀K/DA ALL OUT系列皮肤时触发的，但部署到游戏中变成只在击杀旧版K/DA才会触发。论拳头垃圾般的语音管理'
+			}
+		],
+		'A60DA22A': [
+			{
+				oper: 'extra',
+				mark: '注：这语音应是击杀K/DA ALL OUT系列皮肤时触发的，但部署到游戏中变成只在击杀旧版K/DA才会触发。论拳头垃圾般的语音管理'
+			}
+		],
+		'380E2367': [
+			{
+				oper: 'extra',
+				mark: '注：这语音应是击杀K/DA ALL OUT系列皮肤时触发的，事件是内置对了，但是有笔误了，结果是无法触发￣△￣'
+			}
+		],
+	}
+};
