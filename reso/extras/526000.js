28c6dd262d1276bb7e18459054fedc146c0f6d3a
DanoR <zheung@vip.qq.com>
2020-11-25 14:55:10 +0800

    526

---
+++ reso/extras/526000.js
@@ -0,0 +1,75 @@
+module.exports = {
+	emote: 'b(￣▽￣)d',
+	event: {
+		'[初遇:英雄:卡西奥佩娅]':
+			{ target: '${C.path.project.autogen}reso/icons/Cassiopeia/0.png' },
+		'[击杀:英雄:卡西奥佩娅]':
+			{ target: '${C.path.project.autogen}reso/icons/Cassiopeia/0.png' },
+		'[初遇:英雄:德莱厄斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Darius/0.png' },
+		'[击杀:英雄:德莱厄斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Darius/0.png' },
+		'[初遇:英雄:艾克]':
+			{ target: '${C.path.project.autogen}reso/icons/Ekko/0.png' },
+		'[击杀:英雄:艾克]':
+			{ target: '${C.path.project.autogen}reso/icons/Ekko/0.png' },
+		'[初遇:英雄:伊莉丝]':
+			{ target: '${C.path.project.autogen}reso/icons/Elise/0.png' },
+		'[击杀:英雄:伊莉丝]':
+			{ target: '${C.path.project.autogen}reso/icons/Elise/0.png' },
+		'[初遇:英雄:克烈]':
+			{ target: '${C.path.project.autogen}reso/icons/Kled/0.png' },
+		'[击杀:英雄:克烈]':
+			{ target: '${C.path.project.autogen}reso/icons/Kled/0.png' },
+		'[初遇:英雄:乐芙兰]':
+			{ target: '${C.path.project.autogen}reso/icons/LeBlanc/0.png' },
+		'[击杀:英雄:乐芙兰]':
+			{ target: '${C.path.project.autogen}reso/icons/LeBlanc/0.png' },
+		'[初遇:英雄:蕾欧娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Leona/0.png' },
+		'[击杀:英雄:蕾欧娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Leona/0.png' },
+		'[初遇:英雄:莫德凯撒]':
+			{ target: '${C.path.project.autogen}reso/icons/Mordekaiser/0.png' },
+		'[击杀:英雄:莫德凯撒]':
+			{ target: '${C.path.project.autogen}reso/icons/Mordekaiser/0.png' },
+		'[初遇:英雄:莎弥拉]':
+			{ target: '${C.path.project.autogen}reso/icons/Samira/0.png' },
+		'[击杀:英雄:莎弥拉]':
+			{ target: '${C.path.project.autogen}reso/icons/Samira/0.png' },
+		'[初遇:英雄:萨勒芬妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Seraphine/0.png' },
+		'[击杀:英雄:萨勒芬妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Seraphine/0.png' },
+		'[初遇:英雄:瑟提]':
+			{ target: '${C.path.project.autogen}reso/icons/Sett/0.png' },
+		'[击杀:英雄:瑟提]':
+			{ target: '${C.path.project.autogen}reso/icons/Sett/0.png' },
+		'[初遇:英雄:斯维因]':
+			{ target: '${C.path.project.autogen}reso/icons/Swain/0.png' },
+		'[击杀:英雄:斯维因]':
+			{ target: '${C.path.project.autogen}reso/icons/Swain/0.png' },
+
+		'[使用:Q技能:裂阵]':
+			{ skill: '${C.path.project.autogen}reso/icons/Rell/Q.png' },
+		'[使用:W技能:驭铁术：轰落]':
+			{ skill: '${C.path.project.autogen}reso/icons/Rell/W.png' },
+		'[使用:W技能:驭铁术：驾临]':
+			{ skill: '${C.path.project.autogen}reso/icons/Rell/W_Mount.png' },
+		'[使用:E技能:挥斥]':
+			{ skill: '${C.path.project.autogen}reso/icons/Rell/E.png' },
+		'[使用:R技能:极涌]':
+			{ skill: '${C.path.project.autogen}reso/icons/Rell/R.png' },
+	},
+	lines: {
+		'FA4FDA17': [
+			{
+				oper: 'after',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Ekko/0.png',
+				line: 'ヾ(=ﾟ･ﾟ=)ﾉ',
+			},
+		]
+	}
+};
---
+++ reso/extras/55029.js
@@ -0,0 +1,65 @@
+module.exports = {
+	emote: 'b(￣▽￣)d',
+	event: {
+		'[初遇:英雄:盖伦]':
+			{ target: '${C.path.project.autogen}reso/icons/Garen/0.png' },
+		'[击杀:英雄:盖伦]':
+			{ target: '${C.path.project.autogen}reso/icons/Garen/0.png' },
+		'[击杀:英雄:凯特琳]':
+			{ target: '${C.path.project.autogen}reso/icons/Caitlyn/0.png' },
+		'[初遇:皮肤:女帝 戴安娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Diana/25.png' },
+		'[攻击:皮肤:女帝 戴安娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Diana/25.png' },
+		'[击杀:皮肤:女帝 戴安娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Diana/25.png' },
+		'[初遇:皮肤:女帝 迦娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Janna/27.png' },
+		'[攻击:皮肤:女帝 迦娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Janna/27.png' },
+		'[击杀:皮肤:女帝 迦娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Janna/27.png' },
+		'[初遇:皮肤:女帝 奇亚娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Qiyana/11.png' },
+		'[攻击:皮肤:女帝 奇亚娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Qiyana/11.png' },
+		'[初遇:皮肤:女帝 芮尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Rell/1.png' },
+		'[攻击:皮肤:女帝 芮尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Rell/1.png' },
+		'[击杀:皮肤:女帝 芮尔]':
+			{ target: '${C.path.project.autogen}reso/icons/Rell/1.png' },
+
+		'[使用:Q技能:弹射之刃]':
+			{ skill: '${C.path.project.autogen}reso/icons/Katarina/Q.png' },
+		'[使用:W技能:伺机待发]':
+			{ skill: '${C.path.project.autogen}reso/icons/Katarina/W.png' },
+		'[使用:E技能:瞬步]':
+			{ skill: '${C.path.project.autogen}reso/icons/Katarina/E.png' },
+		'[使用:R技能:死亡莲华]':
+			{ skill: '${C.path.project.autogen}reso/icons/Katarina/R.png' },
+	},
+	lines: {
+		'03CF606A': [
+			{
+				oper: 'extra',
+				mark: '注：每参与击杀3次解锁一把新刀刃'
+			}
+		],
+		'521D472B': [
+			{
+				oper: 'extra',
+				mark: '注：每参与击杀3次解锁一把新刀刃'
+			}
+		],
+		'6DA1AA36': [
+			{
+				oper: 'after',
+				side: 'left',
+				duration: 0.4,
+				head: '${C.path.project.autogen}reso/icons/Garen/0.png',
+				line: '(╥╯^╰╥)',
+			},
+		]
+	}
+};
