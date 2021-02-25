0a1e64edccda344b1fc94171a62f0c4628d2dfcd
DanoR <zheung@vip.qq.com>
2021-02-25 12:05:05 +0800

    sp02

--- reso/extras/sp02_yone_related_voices.js
+++ reso/extras/sp02_yone_related_voices.js
@@ -1,5 +1,8 @@
 module.exports = {
+	emote: '└（★ｏ★）┐',
 	event: {
+		'[初遇:英雄:亚索]':
+			{ target: '${C.path.project.autogen}reso/icons/Yasuo/0.png' },
 		'[初遇:英雄:永恩]':
 			{ target: '${C.path.project.autogen}reso/icons/Yone/0.png' },
 		'[击杀:英雄:永恩]':
@@ -24,15 +27,15 @@ module.exports = {
 			arrLine: [
 				{
 					line: '永恩，我领会了你的教诲，但为时已晚',
-					crc32: 'DF81C9EA', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					crc32: '002A752F', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 				{
-					line: '不配出鞘的剑，遇上不详的面具',
-					crc32: '45552CA0', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					line: '不配出鞘的剑，遇上了不详的面具',
+					crc32: 'A9769161', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 				{
-					line: '哥哥？你，怎么了？',
-					crc32: '47B8FAEE', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					line: '哥哥，你，怎么了？',
+					crc32: 'D543F41D', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 			],
 		},
@@ -42,7 +45,7 @@ module.exports = {
 			arrLine: [
 				{
 					line: '又是带血的结局，哥哥',
-					crc32: 'A41675A8', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					crc32: 'BC5D2112', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 			],
 		},
@@ -52,11 +55,12 @@ module.exports = {
 			arrLine: [
 				{
 					line: '我哥哥回来了。看来赴死也并非易如反掌了',
-					crc32: '0C7890EA', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					crc32: '2D2E2721', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 				{
 					line: '清风如窃，诉说着一个带着面具的双持剑客',
-					crc32: '0D1F7984', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					crc32: '48017374', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
+					mark: '原文“Whispers in the wind”，风中低语。是窃窃私语的“窃”'
 				},
 			],
 		},
@@ -65,18 +69,70 @@ module.exports = {
 			eventRaw: 'Move2DLong',
 			arrLine: [
 				{
-					line: '我本以为亚扎卡纳不过是，孩童的故事而已，真是大错特错',
-					crc32: '030EFDFD', side: 'right', audioFolder: 'Yasuo@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png'
+					line: '我本以为亚扎卡纳不过是孩童的故事而已，真是大错特错',
+					crc32: '0506260B', side: 'right', audioFolder: '[157000]疾风剑豪 亚索[Yasuo@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Yasuo/0.png',
 				},
 			],
 		},
+
+		{
+			event: '[初遇:英雄:亚索]',
+			eventRaw: 'FirstEncounter3DYone',
+			arrLine: [
+				{
+					line: '他一说话，我听见的就是你的呢喃。梦啊',
+					crc32: 'A570C436', side: 'right', audioFolder: '[876000]含羞蓓蕾 莉莉娅[Lillia@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Lillia/0.png',
+				},
+			],
+		},
+		{
+			event: '[初遇:英雄:亚索]',
+			eventRaw: 'FirstEncounter3DYone',
+			arrLine: [
+				{
+					line: '逃避不会解决问题，但是能然我好过一些',
+					crc32: 'ED266267', side: 'right', audioFolder: '[876000]含羞蓓蕾 莉莉娅[Lillia@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Lillia/0.png',
+				},
+			],
+		},
+		{
+			event: '[初遇:英雄:永恩]',
+			eventRaw: 'FirstEncounter3DYone',
+			arrLine: [
+				{
+					line: '梦要是被恐惧所囚禁，就会变成噩梦。我们在对抗同样的敌人',
+					crc32: '68D83E65', side: 'right', audioFolder: '[876000]含羞蓓蕾 莉莉娅[Lillia@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Lillia/0.png',
+				},
+			],
+		},
+		{
+			event: '[初遇:英雄:永恩]',
+			eventRaw: 'FirstEncounter3DYone',
+			arrLine: [
+				{
+					line: '我能感受你的渴望，而在他的心底也是一样的',
+					crc32: 'AF925443', side: 'right', audioFolder: '[876000]含羞蓓蕾 莉莉娅[Lillia@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Lillia/0.png',
+				},
+			],
+		},
+		{
+			event: '[初遇:英雄:永恩]',
+			eventRaw: 'FirstEncounter3DYone',
+			arrLine: [
+				{
+					line: '永恩，你为什么想要摘掉面具呢？不是很方便你藏起来吗？',
+					crc32: 'CB5E1E0B', side: 'right', audioFolder: '[876000]含羞蓓蕾 莉莉娅[Lillia@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Lillia/0.png',
+				},
+			],
+		},
+
 		{
 			event: '[初遇:皮肤:灵魂莲华 亚索]',
 			eventRaw: 'FirstEncounter3DYasuoSkin35',
 			arrLine: [
 				{
 					line: '你好，亚索。你还是没搞清楚，落叶该去何处吗？',
-					crc32: '7167BDB5', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					crc32: '86A81714', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
@@ -85,8 +141,8 @@ module.exports = {
 			eventRaw: 'Attack3DYasuoSkin35',
 			arrLine: [
 				{
-					line: '哈撒给',
-					crc32: '885C5454', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					line: '(哈撒给)',
+					crc32: 'EB86A73D', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
@@ -96,7 +152,7 @@ module.exports = {
 			arrLine: [
 				{
 					line: '死亡如风',
-					crc32: '873DE756', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					crc32: '57747146', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
@@ -106,7 +162,7 @@ module.exports = {
 			arrLine: [
 				{
 					line: '永恩，别担心。我觉得你和你弟弟一样潇洒',
-					crc32: '74DBA756', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					crc32: '6E434054', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
@@ -117,7 +173,7 @@ module.exports = {
 			arrLine: [
 				{
 					line: '你是哥哥，还是弟弟来着？',
-					crc32: '34E0F745', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					crc32: '74625A0B', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
@@ -127,7 +183,7 @@ module.exports = {
 			arrLine: [
 				{
 					line: '我接受你的投降，永恩',
-					crc32: 'CC62CEB1', side: 'right', audioFolder: 'Ahri@zh_cn@PBE1', head: '${C.path.project.autogen}reso/icons/Ahri/27.png'
+					crc32: '6BA5EA61', side: 'right', audioFolder: '[103027]灵魂莲华 阿狸[Ahri@PBE1@zh_cn]', head: '${C.path.project.autogen}reso/icons/Ahri/27.png',
 				},
 			],
 		},
