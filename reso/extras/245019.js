d4d8571ef2362635172ecf7a39d0306a5739c284
DanoR <zheung@vip.qq.com>
2021-03-02 15:52:47 +0800

    td ekko

---
+++ reso/extras/245019.js
@@ -0,0 +1,69 @@
+module.exports = {
+	emote: 'w(ﾟДﾟ)w',
+	event: {
+		'[初遇:英雄:艾克]': {
+			target: '${C.path.project.autogen}reso/icons/ekko/0.png',
+		},
+		'[击杀:英雄:艾克]': {
+			target: '${C.path.project.autogen}reso/icons/ekko/0.png',
+		},
+		'[初遇:皮肤:DJ娑娜]': {
+			target: '${C.path.project.autogen}reso/icons/sona/6.png',
+		},
+		'[击杀:皮肤:DJ娑娜]': {
+			target: '${C.path.project.autogen}reso/icons/sona/6.png',
+		},
+		'[初遇:皮肤:真实伤害 阿卡丽]': {
+			target: '${C.path.project.autogen}reso/icons/akali/15.png',
+		},
+		'[击杀:皮肤:真实伤害 阿卡丽]': {
+			target: '${C.path.project.autogen}reso/icons/akali/15.png',
+		},
+		'[初遇:皮肤:真实伤害 奇亚娜]': {
+			target: '${C.path.project.autogen}reso/icons/qiyana/2.png',
+		},
+		'[击杀:皮肤:真实伤害 奇亚娜]': {
+			target: '${C.path.project.autogen}reso/icons/qiyana/2.png',
+		},
+		'[初遇:皮肤:真实伤害 赛娜]': {
+			target: '${C.path.project.autogen}reso/icons/senna/1.png',
+		},
+		'[击杀:皮肤:真实伤害 赛娜]': {
+			target: '${C.path.project.autogen}reso/icons/senna/1.png',
+		},
+		'[初遇:皮肤:真实伤害 亚索]': {
+			target: '${C.path.project.autogen}reso/icons/yasuo/18.png',
+		},
+		'[击杀:皮肤:真实伤害 亚索]': {
+			target: '${C.path.project.autogen}reso/icons/yasuo/18.png',
+		},
+
+		'[使用:Q技能:时间卷曲器]': {
+			skill: '${C.path.project.autogen}reso/icons/Ekko/Q.png'
+		},
+		'[使用:W技能:时光交错]': {
+			skill: '${C.path.project.autogen}reso/icons/Ekko/W.png'
+		},
+		'[使用:E技能:相位俯冲]': {
+			skill: '${C.path.project.autogen}reso/icons/Ekko/E.png'
+		},
+		'[使用:R技能:时空断裂]': {
+			skill: '${C.path.project.autogen}reso/icons/Ekko/R.png'
+		},
+	},
+	lines: {
+		'0C444C42': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icons/Lucian/0.png',
+					line: '(눈_눈)',
+					hideEvent: true,
+				},
+			],
+		},
+		'44481039': { mark: '不！是！Uncle！' },
+		'DCCD212C': { mark: '不！是！Uncle！' },
+
+	}
+};
