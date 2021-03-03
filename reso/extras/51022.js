8bc207f87973603e91426ef5c7065066eb500eb0
DanoR <zheung@vip.qq.com>
2021-03-03 11:36:24 +0800

    New `Battle Academia Caitlyn`'s extra

---
+++ reso/extras/51022.js
@@ -0,0 +1,23 @@
+module.exports = {
+	emote: '✧(≖ ◡ ≖✿)',
+	event: {
+
+		'[使用:P被动:爆头:命中]': {
+			skill: '${C.path.project.autogen}reso/icons/Caitlyn/Q.png'
+		},
+		'[使用:Q技能:和平使者]': {
+			skill: '${C.path.project.autogen}reso/icons/Caitlyn/Q.png'
+		},
+		'[使用:W技能:约德尔诱捕器:陷阱命中]': {
+			skill: '${C.path.project.autogen}reso/icons/Caitlyn/W.png'
+		},
+		'[使用:E技能:90口径绳网]': {
+			skill: '${C.path.project.autogen}reso/icons/Caitlyn/E.png'
+		},
+		'[使用:R技能:让子弹飞]': {
+			skill: '${C.path.project.autogen}reso/icons/Caitlyn/R.png'
+		},
+	},
+	lines: {
+	}
+};
