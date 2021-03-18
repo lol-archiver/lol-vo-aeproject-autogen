5b41e1f9ab153b549f1c770cb18da27f82b1640c
DanoR <zheung@vip.qq.com>
2021-03-18 09:51:14 +0800

    51022

--- footage.js
+++ footage.js
@@ -4,7 +4,13 @@ this.F = function Footage(path, parent) {
 	var footage = mapFootage[path];
 
 	if(!footage) {
-		footage = app.project.importFile(new ImportOptions(new File(path)));
+		// eslint-disable-next-line no-useless-catch
+		try {
+			footage = app.project.importFile(new ImportOptions(new File(path)));
+		}
+		catch(error) {
+			throw error;
+		}
 
 		if(parent) {
 			footage.parentFolder = parent;
--- reso/extras/51022.js
+++ reso/extras/51022.js
@@ -1,6 +1,66 @@
 module.exports = {
 	emote: '✧(≖ ◡ ≖✿)',
 	event: {
+		'[初遇:英雄:斯维因]': {
+			target: '${C.path.project.autogen}reso/icons/Swain/0.png'
+		},
+		'[击杀:英雄:斯维因]': {
+			target: '${C.path.project.autogen}reso/icons/Swain/0.png'
+		},
+		'[初遇:皮肤:战斗学院 伊泽瑞尔]': {
+			target: '${C.path.project.autogen}reso/icons/Ezreal/21.png'
+		},
+		'[击杀:皮肤:战斗学院 伊泽瑞尔]': {
+			target: '${C.path.project.autogen}reso/icons/Ezreal/21.png'
+		},
+		'[初遇:皮肤:战斗学院 盖伦]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Garen.png'
+		},
+		'[击杀:皮肤:战斗学院 盖伦]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Garen.png'
+		},
+		'[初遇:皮肤:战斗学院 杰斯]': {
+			target: '${C.path.project.autogen}reso/icons/Jayce/5.png'
+		},
+		'[击杀:皮肤:战斗学院 杰斯]': {
+			target: '${C.path.project.autogen}reso/icons/Jayce/5.png'
+		},
+		'[初遇:皮肤:战斗学院 卡特琳娜]': {
+			target: '${C.path.project.autogen}reso/icons/Katarina/12.png'
+		},
+		'[击杀:皮肤:战斗学院 卡特琳娜]': {
+			target: '${C.path.project.autogen}reso/icons/Katarina/12.png'
+		},
+		'[初遇:皮肤:战斗学院 蕾欧娜]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Leona.png'
+		},
+		'[击杀:皮肤:战斗学院 蕾欧娜]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Leona.png'
+		},
+		'[初遇:皮肤:战斗学院 拉克丝]': {
+			target: '${C.path.project.autogen}reso/icons/Lux/15.png'
+		},
+		'[击杀:皮肤:战斗学院 拉克丝]': {
+			target: '${C.path.project.autogen}reso/icons/Lux/15.png'
+		},
+		'[初遇:皮肤:战斗学院 悟空]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Wukong.png'
+		},
+		'[击杀:皮肤:战斗学院 悟空]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Wukong.png'
+		},
+		'[初遇:皮肤:战斗学院 永恩]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Yone.png'
+		},
+		'[击杀:皮肤:战斗学院 永恩]': {
+			target: '${C.path.project.autogen}reso/others/icons/BA_Yone.png'
+		},
+		'[初遇:皮肤:战斗校长 悠米]': {
+			target: '${C.path.project.autogen}reso/icons/Yuumi/1.png'
+		},
+		'[击杀:皮肤:战斗校长 悠米]': {
+			target: '${C.path.project.autogen}reso/icons/Yuumi/1.png'
+		},
 
 		'[使用:P被动:爆头:命中]': {
 			skill: '${C.path.project.autogen}reso/icons/Caitlyn/Q.png'
@@ -19,5 +79,15 @@ module.exports = {
 		},
 	},
 	lines: {
+		'8F6DF620': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icons/Jayce/5.png',
+					line: '（＃￣～￣＃）',
+					hideEvent: true,
+				},
+			],
+		}
 	}
 };
