f213d936b3f2ab4319aebc769d3af99177b0b182
DanoR <zheung@vip.qq.com>
2021-05-12 16:39:31 +0800

    82013

---
+++ reso/extras/82013.js
@@ -0,0 +1,79 @@
+module.exports = {
+	emote: '(ФωФ)',
+	event: {
+		'[初遇:皮肤:源计划：联合 艾希]':
+			{ target: '${C.path.project.autogen}reso/icons/Ashe/8.png' },
+		'[击杀:皮肤:源计划：联合 艾希]':
+			{ target: '${C.path.project.autogen}reso/icons/Ashe/8.png' },
+		'[初遇:皮肤:源代码 丽桑卓]':
+			{ target: '${C.path.project.autogen}reso/icons/Lissandra/3.png' },
+		'[击杀:皮肤:源代码 丽桑卓]':
+			{ target: '${C.path.project.autogen}reso/icons/Lissandra/3.png' },
+		'[初遇:皮肤:源计划：林 易]':
+			{ target: '${C.path.project.autogen}reso/icons/MasterYi/9.png' },
+		'[击杀:皮肤:源计划：林 易]':
+			{ target: '${C.path.project.autogen}reso/icons/MasterYi/9.png' },
+		'[初遇:皮肤:源计划：毁灭 派克]':
+			{ target: '${C.path.project.autogen}reso/icons/Pyke/16.png' },
+		'[击杀:皮肤:源计划：毁灭 派克]':
+			{ target: '${C.path.project.autogen}reso/icons/Pyke/16.png' },
+		'[初遇:皮肤:源计划 雷克顿]':
+			{ target: '${C.path.project.autogen}reso/icons/Renekton/26.png' },
+		'[击杀:皮肤:源计划 雷克顿]':
+			{ target: '${C.path.project.autogen}reso/icons/Renekton/26.png' },
+		'[嘲讽:友方:英雄:瑟庄妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Sejuani/16.png' },
+		'[初遇:皮肤:源计划 瑟庄妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Sejuani/16.png' },
+		'[击杀:皮肤:源计划 瑟庄妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Sejuani/16.png' },
+		'[初遇:皮肤:源计划 赛娜]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Senna.png' },
+		'[击杀:皮肤:源计划 赛娜]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Senna.png' },
+		'[初遇:皮肤:源计划 塞拉斯]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Sylas.png' },
+		'[击杀:皮肤:源计划 塞拉斯]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Sylas.png' },
+		'[初遇:皮肤:源计划 维鲁斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Varus/16.png' },
+		'[击杀:皮肤:源计划 维鲁斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Varus/16.png' },
+		'[初遇:皮肤:源计划：净化 薇恩]':
+			{ target: '${C.path.project.autogen}reso/icons/Vayne/11.png' },
+		'[击杀:皮肤:源计划：净化 薇恩]':
+			{ target: '${C.path.project.autogen}reso/icons/Vayne/11.png' },
+
+		'[使用:Q技能:破灭之锤]':
+			{ skill: '${C.path.project.autogen}reso/icons/Mordekaiser/Q.png' },
+		'[使用:W技能:不坏之身]':
+			{ skill: '${C.path.project.autogen}reso/icons/Mordekaiser/W.png' },
+		'[使用:E技能:断魂一扼]':
+			{ skill: '${C.path.project.autogen}reso/icons/Mordekaiser/E.png' },
+		'[使用:R技能:轮回绝境]':
+			{ skill: '${C.path.project.autogen}reso/icons/Mordekaiser/R.png' },
+	},
+	lines: {
+		'D92FBF3B': {
+			mark: '机器人学第一定律：“机器人不得伤害人类个体，或者目睹人类个体将遭受危险而袖手不管”',
+		},
+		'AC27BB4A': {
+			mark: '机器人学第二定律：“机器人必须服从人给予它的命令，当该命令与第一定律冲突时例外”',
+		},
+		'30306955': {
+			mark: '机器人学第三定律：“机器人在不违反第一、第二定律的情况下要尽可能保护自己的生存”',
+		},
+
+
+		'B9DF1014': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icons/Varus/16.png',
+					line: '01001100011001010110000101101011\n0110111101100110\n01001100011010010110111101110100',
+					hideEvent: true,
+				},
+			]
+		},
+	}
+};
