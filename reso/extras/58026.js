086da9d99b4ca0102feae8cc3436e4f4640e3623
DanoR <zheung@vip.qq.com>
2021-05-14 16:42:58 +0800

    58026

---
+++ reso/extras/58026.js
@@ -0,0 +1,80 @@
+module.exports = {
+	emote: '(ФωФ)',
+	event: {
+		'[初遇:英雄:内瑟斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Nasus/8.png' },
+		'[击杀:英雄:内瑟斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Nasus/8.png' },
+		'[初遇:皮肤:源计划：超体 莫德凯撒]':
+			{ target: '${C.path.project.autogen}reso/icons/Morderkasai/13.png' },
+		'[击杀:皮肤:源计划：超体 莫德凯撒]':
+			{ target: '${C.path.project.autogen}reso/icons/Morderkasai/13.png' },
+		'[初遇:皮肤:源计划：末日 沃里克]':
+			{ target: '${C.path.project.autogen}reso/icons/Warwick/16.png' },
+		'[击杀:皮肤:源计划：末日 沃里克]':
+			{ target: '${C.path.project.autogen}reso/icons/Warwick/16.png' },
+
+		'[初遇:皮肤:源计划：联合 艾希]':
+			{ target: '${C.path.project.autogen}reso/icons/Ashe/8.png' },
+		'[击杀:皮肤:源计划：联合 艾希]':
+			{ target: '${C.path.project.autogen}reso/icons/Ashe/8.png' },
+		'[初遇:皮肤:源计划：林 易]':
+			{ target: '${C.path.project.autogen}reso/icons/MasterYi/9.png' },
+		'[击杀:皮肤:源计划：林 易]':
+			{ target: '${C.path.project.autogen}reso/icons/MasterYi/9.png' },
+		'[初遇:皮肤:源计划：毁灭 派克]':
+			{ target: '${C.path.project.autogen}reso/icons/Pyke/16.png' },
+		'[击杀:皮肤:源计划：毁灭 派克]':
+			{ target: '${C.path.project.autogen}reso/icons/Pyke/16.png' },
+		'[初遇:皮肤:源计划：泯灭 雷克顿]':
+			{ target: '${C.path.project.autogen}reso/icons/Renekton/26.png' },
+		'[击杀:皮肤:源计划：泯灭 雷克顿]':
+			{ target: '${C.path.project.autogen}reso/icons/Renekton/26.png' },
+		'[初遇:皮肤:源计划：统帅 瑟庄妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Sejuani/16.png' },
+		'[击杀:皮肤:源计划：统帅 瑟庄妮]':
+			{ target: '${C.path.project.autogen}reso/icons/Sejuani/16.png' },
+		'[初遇:皮肤:源计划：魅灵 赛娜]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Senna.png' },
+		'[击杀:皮肤:源计划：魅灵 赛娜]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Senna.png' },
+		'[初遇:皮肤:源计划：逆流 塞拉斯]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Sylas.png' },
+		'[击杀:皮肤:源计划：逆流 塞拉斯]':
+			{ target: '${C.path.project.autogen}reso/others/icons/PROJ_Sylas.png' },
+		'[初遇:皮肤:源计划：天眼 维鲁斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Varus/16.png' },
+		'[击杀:皮肤:源计划：天眼 维鲁斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Varus/16.png' },
+
+		'[使用:Q技能:暴君狂击]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/Q.png' },
+		'[使用:W技能:冷酷捕猎]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/W.png' },
+		'[使用:W技能:冷酷捕猎:狂怒状态下]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/W.png' },
+		'[使用:E技能:横冲直撞:横冲]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/E.png' },
+		'[使用:E技能:横冲直撞:直撞]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/E.png' },
+		'[使用:R技能:终极统治]':
+			{ skill: '${C.path.project.autogen}reso/icons/Renekton/R.png' },
+	},
+	lines: {
+		'00E24904': {
+			mark: '此处应该还有一句语音，但中文对应的文件是空的',
+		},
+
+
+		'EE47DF4B': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icons/Varus/16.png',
+					line: 'o(≧口≦)o\n￣へ￣\n╰(‵□′)╯',
+					hideEvent: true,
+				},
+			]
+		},
+	}
+};
