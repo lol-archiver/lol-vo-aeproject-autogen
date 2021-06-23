22f806b78a895166cdeec27e355a103bd313c08b
DanoR <zheung@vip.qq.com>
2021-06-23 14:12:15 +0800

    067025

---
+++ reso/conf/067025.js
@@ -0,0 +1,101 @@
+module.exports = {
+	title: '光明哨兵 薇恩',
+	champion: {
+		id: 67,
+		name: 'Vayne',
+	},
+	skin: {
+		id: 25,
+		name: 'Sentinel'
+	},
+	emote: 'ψ( ╰_╯ )',
+	events: {
+		'[助攻:英雄:格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/0.png' },
+		'[移动:初次:皮肤:光明哨兵 格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/35.png' },
+		'[初遇:英雄:格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/0.png' },
+		'[击杀:英雄:格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/0.png' },
+		'[初遇:皮肤:光明哨兵 格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/35.png' },
+		'[击杀:皮肤:光明哨兵 格雷福斯]':
+			{ target: '${C.path.project.autogen}reso/icons/Graves/35.png' },
+		'[初遇:皮肤:光明哨兵 血港鬼影]':
+			{ target: '${C.path.project.autogen}reso/icons/Pyke/34.png' },
+		'[初遇:皮肤:破败军团 厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icons/MissFortune/21.png' },
+		'[初遇:皮肤:光明哨兵 皎月女神]':
+			{ target: '${C.path.project.autogen}reso/icons/Diana/28.png' },
+		'[初遇:皮肤:光明哨兵 艾瑞莉娅]':
+			{ target: '${C.path.project.autogen}reso/icons/Irelia/26.png' },
+		'[初遇:皮肤:光明哨兵 奥拉夫]':
+			{ target: '${C.path.project.autogen}reso/icons/Olaf/25.png' },
+		'[初遇:皮肤:光明哨兵 锐雯]':
+			{ target: '${C.path.project.autogen}reso/icons/Riven/34.png' },
+		'[初遇:皮肤:破败军团 潘森]':
+			{ target: '${C.path.project.autogen}reso/icons/Pantheon/25.png' },
+		'[初遇:皮肤:破败军团 德莱文]':
+			{ target: '${C.path.project.autogen}reso/icons/Draven/20.png' },
+		'[初遇:皮肤:破败军团 卡尔玛]':
+			{ target: '${C.path.project.autogen}reso/icons/Karma/27.png' },
+		'[初遇:皮肤:破败军团 希瓦娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Shyvanna/8.png' },
+		'[击杀:皮肤:破败军团 希瓦娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Shyvanna/8.png' },
+		'[初遇:英雄:佛耶戈]':
+			{ target: '${C.path.project.autogen}reso/icons/Viego/0.png' },
+		'[击杀:英雄:佛耶戈]':
+			{ target: '${C.path.project.autogen}reso/icons/Viego/0.png' },
+		'[初遇:英雄:格温]':
+			{ target: '${C.path.project.autogen}reso/icons/Gwen/0.png' },
+		'[击杀:英雄:格温]':
+			{ target: '${C.path.project.autogen}reso/icons/Gwen/0.png' },
+		'[初遇:英雄:卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icons/Lucian/0.png' },
+		'[击杀:英雄:卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icons/Lucian/0.png' },
+		'[初遇:英雄:赛娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Senna/0.png' },
+		'[击杀:英雄:赛娜]':
+			{ target: '${C.path.project.autogen}reso/icons/Senna/0.png' },
+		'[初遇:英雄:波比]':
+			{ target: '${C.path.project.autogen}reso/icons/Poppy/0.png' },
+		'[击杀:英雄:波比]':
+			{ target: '${C.path.project.autogen}reso/icons/Poppy/0.png' },
+		'[初遇:英雄:伊芙琳]':
+			{ target: '${C.path.project.autogen}reso/icons/Evelynn/0.png' },
+		'[击杀:英雄:伊芙琳]':
+			{ target: '${C.path.project.autogen}reso/icons/Evelynn/0.png' },
+
+		'[使用:Q技能:闪避突袭]':
+			{ skill: '${C.path.project.autogen}reso/icons/Vayne/Q.png' },
+		'[使用:E技能:恶魔审判]':
+			{ skill: '${C.path.project.autogen}reso/icons/Vayne/E.png' },
+		'[使用:E技能:恶魔审判:击退至地形]':
+			{ skill: '${C.path.project.autogen}reso/icons/Vayne/E.png' },
+		'[使用:R技能:终极时刻]':
+			{ skill: '${C.path.project.autogen}reso/icons/Vayne/R.png' },
+	},
+	lines: {
+		'FC198474': {
+			mark: '该语音是提前预置的语音，皮肤将在下一版本11.15退出',
+		},
+		'778D9E37': {
+			mark: '该语音是提前预置的语音，皮肤将在下一版本11.15退出',
+		},
+
+
+		'176A745C': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icons/Olaf/25.png',
+					line: 'ヽ(💢｀皿´💢)ノ',
+					hideEvent: true,
+				},
+			]
+		},
+	}
+};
