63a315607024ff197bcd85d93f6352070273cdfa
DanoR <zheung@vip.qq.com>
2021-07-08 16:49:30 +0800

    104035pre

---
+++ reso/conf/104035.js
@@ -0,0 +1,116 @@
+module.exports = {
+	title: '光明哨兵 格雷福斯',
+	champion: {
+		id: 104,
+		name: 'Graves',
+	},
+	skin: {
+		id: 035,
+		name: 'Sentinel'
+	},
+	emote: 'ψ( ╰_╯ )',
+	events: {
+		'[助攻:英雄:薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/0.png' },
+		'[初遇:英雄:薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/0.png' },
+		'[击杀:英雄:薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/0.png' },
+		'[移动:初次][友方:使用:皮肤:光明哨兵 薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/25.png' },
+		'[初遇:皮肤:光明哨兵 薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/25.png' },
+		'[击杀:皮肤:光明哨兵 薇恩]':
+			{ target: '${C.path.project.autogen}reso/icon/Vayne/25.png' },
+		'[初遇:英雄:崔斯特]':
+			{ target: '${C.path.project.autogen}reso/icon/TwistFate/0.png' },
+		'[击杀:英雄:崔斯特]':
+			{ target: '${C.path.project.autogen}reso/icon/TwistFate/0.png' },
+		'[初遇:皮肤:光明哨兵 黛安娜]':
+			{ target: '${C.path.project.autogen}reso/icon/Diana/28.png' },
+		'[初遇:皮肤:光明哨兵 艾瑞莉娅]':
+			{ target: '${C.path.project.autogen}reso/icon/Irelia/26.png' },
+		'[初遇:皮肤:光明哨兵 奥拉夫]':
+			{ target: '${C.path.project.autogen}reso/icon/Olaf/25.png' },
+		'[初遇:皮肤:光明哨兵 派克]':
+			{ target: '${C.path.project.autogen}reso/icon/Pyke/34.png' },
+		'[击杀:皮肤:光明哨兵 派克]':
+			{ target: '${C.path.project.autogen}reso/icon/Pyke/34.png' },
+		'[初遇:皮肤:光明哨兵 雷恩加尔]':
+			{ target: '${C.path.project.autogen}reso/icon/Rengar/30.png' },
+		'[初遇:皮肤:光明哨兵 锐雯]':
+			{ target: '${C.path.project.autogen}reso/icon/Riven/34.png' },
+		'[初遇:皮肤:破败军团 德莱文]':
+			{ target: '${C.path.project.autogen}reso/icon/Draven/20.png' },
+		'[初遇:皮肤:破败军团 卡尔玛]':
+			{ target: '${C.path.project.autogen}reso/icon/Karma/27.png' },
+		'[初遇:皮肤:破败军团 厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icon/MissFortune/21.png' },
+		'[击杀:皮肤:破败军团 厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icon/MissFortune/21.png' },
+		'[初遇:皮肤:破败军团 潘森]':
+			{ target: '${C.path.project.autogen}reso/icon/Pantheon/25.png' },
+		'[初遇:皮肤:破败军团 希瓦娜]':
+			{ target: '${C.path.project.autogen}reso/icon/Shyvana/8.png' },
+		'[初遇:英雄:佛耶戈]':
+			{ target: '${C.path.project.autogen}reso/icon/Viego/0.png' },
+		'[击杀:英雄:佛耶戈]':
+			{ target: '${C.path.project.autogen}reso/icon/Viego/0.png' },
+		'[初遇:英雄:格温]':
+			{ target: '${C.path.project.autogen}reso/icon/Gwen/0.png' },
+		'[击杀:英雄:格温]':
+			{ target: '${C.path.project.autogen}reso/icon/Gwen/0.png' },
+		'[初遇:英雄:卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/0.png' },
+		'[击杀:英雄:卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/0.png' },
+		'[初遇:英雄:赛娜]':
+			{ target: '${C.path.project.autogen}reso/icon/Senna/0.png' },
+		'[击杀:英雄:赛娜]':
+			{ target: '${C.path.project.autogen}reso/icon/Senna/0.png' },
+		'[初遇:英雄:约里克]':
+			{ target: '${C.path.project.autogen}reso/icon/Yorick/0.png' },
+		'[初遇:英雄:俄洛伊]':
+			{ target: '${C.path.project.autogen}reso/icon/Illaoi/0.png' },
+
+		'[使用: Q技能: 穷途末路]':
+			{ skill: '${C.path.project.autogen}reso/icon/Graves/Q.png' },
+		'[使用: W技能: 烟幕弹]':
+			{ skill: '${C.path.project.autogen}reso/icon/Graves/W.png' },
+		'[使用: E技能: 快速拔枪]':
+			{ skill: '${C.path.project.autogen}reso/icon/Graves/E.png' },
+		'[使用: R技能: 终极爆弹]':
+			{ skill: '${C.path.project.autogen}reso/icon/Graves/R.png' },
+	},
+	lines: {
+		'雷哥儿': {
+			mark: '雷哥儿对应英文语音中的Rango，是格雷福斯对雷恩加尔（Rengar）的昵称',
+		},
+		'德莱文': {
+			mark: '引申自2014年的CG影片《新的曙光》，影片中格雷福斯攻击德莱文后马上被雷恩加尔击杀了',
+		},
+		'便携烟雾': {
+			mark: 'Pocket smoke!. A reference to Dale Gribble`s blinding technique from King of the Hill: https://youtu.be/PTAXUYLbFYk',
+		},
+		'初见派克': {
+			mark: '派克被俄洛伊的魔法迷惑住了，所以派克不会谋害哨兵',
+		},
+		'击杀派克': {
+			mark: '引申自派克的语音中提到他的死亡是被其他人切断绳子',
+		},
+		'圣光闪瞎你': {
+			mark: 'Blinded by the light!. A reference to songs by both Bruce Springsteen/Manfred Mann and The Weeknd.',
+		},
+
+		'FAE83024': {
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icon/Ezreal/0.png',
+					line: '(o゜▽゜)o☆',
+					hideEvent: true,
+				},
+			],
+		},
+	},
+};
