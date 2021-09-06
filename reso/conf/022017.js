9c8da5f6e16235e7c122d5c2b06352bd5c1c773e
Nuogz <zheung@vip.qq.com>
2021-09-06 16:21:11 +0800

    preconf 022017

---
+++ reso/conf/022017.js
@@ -0,0 +1,99 @@
+module.exports = {
+	title: '西部天使 艾希',
+	champion: {
+		id: 22,
+		name: 'Ashe',
+	},
+	skin: {
+		id: 17,
+		name: 'High Noon'
+	},
+	emote: '(ФωФ)',
+	events: {
+		'[攻击:英雄:凯尔]':
+			{ target: '${C.path.project.autogen}reso/icon/Kayle/0.png' },
+		'[攻击:英雄:莫甘娜]':
+			{ target: '${C.path.project.autogen}reso/icon/Morgana/0.png' },
+		'[初遇:皮肤:荒野镖客 阿利斯塔]':
+			{ target: '${C.path.project.autogen}reso/icon/Alistar/4.png' },
+		'[击杀:皮肤:荒野镖客 阿利斯塔]':
+			{ target: '${C.path.project.autogen}reso/icon/Alistar/4.png' },
+		'[初遇:皮肤:荒野治安官 凯特琳]':
+			{ target: '${C.path.project.autogen}reso/icon/Caitlyn/2.png' },
+		'[击杀:皮肤:荒野治安官 凯特琳]':
+			{ target: '${C.path.project.autogen}reso/icon/Caitlyn/2.png' },
+		'[初遇:皮肤:荒漠之咬 卡西奥佩娅]':
+			{ target: '${C.path.project.autogen}reso/icon/Cassiopeia/1.png' },
+		'[击杀:皮肤:荒漠之咬 卡西奥佩娅]':
+			{ target: '${C.path.project.autogen}reso/icon/Cassiopeia/1.png' },
+		'[初遇:皮肤:西部魔影 德莱厄斯]':
+			{ target: '${C.path.project.autogen}reso/icon/Darius/16.png' },
+		'[攻击:皮肤:西部魔影 德莱厄斯]':
+			{ target: '${C.path.project.autogen}reso/icon/Darius/16.png' },
+		'[击杀:皮肤:西部魔影 德莱厄斯]':
+			{ target: '${C.path.project.autogen}reso/icon/Darius/16.png' },
+		'[初遇:皮肤:荒野幽灵 费德提克]':
+			{ target: '${C.path.project.autogen}reso/icon/Fiddlesticks/3.png' },
+		'[击杀:皮肤:荒野幽灵 费德提克]':
+			{ target: '${C.path.project.autogen}reso/icon/Fiddlesticks/3.png' },
+		'[初遇:皮肤:西部魔影 赫卡里姆]':
+			{ target: '${C.path.project.autogen}reso/icon/Hecarim/8.png' },
+		'[攻击:皮肤:西部魔影 赫卡里姆]':
+			{ target: '${C.path.project.autogen}reso/icon/Hecarim/8.png' },
+		'[击杀:皮肤:西部魔影 赫卡里姆]':
+			{ target: '${C.path.project.autogen}reso/icon/Hecarim/8.png' },
+		'[初遇:皮肤:西部牛仔 烬]':
+			{ target: '${C.path.project.autogen}reso/icon/Jhin/1.png' },
+		'[击杀:皮肤:西部牛仔 烬]':
+			{ target: '${C.path.project.autogen}reso/icon/Jhin/1.png' },
+		'[初遇:皮肤:西部魔影 卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/8.png' },
+		'[攻击:皮肤:西部魔影 卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/8.png' },
+		'[击杀:皮肤:西部魔影 卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/8.png' },
+		'[回应:玩笑:皮肤:西部魔影 卢锡安]':
+			{ target: '${C.path.project.autogen}reso/icon/Lucian/8.png' },
+		'[初遇:皮肤:西部牛仔 厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icon/MissFortune/1.png' },
+		'[击杀:皮肤:西部牛仔 厄运小姐]':
+			{ target: '${C.path.project.autogen}reso/icon/MissFortune/1.png' },
+		'[初遇:皮肤:西部魔影 锤石]':
+			{ target: '${C.path.project.autogen}reso/icon/Thresh/6.png' },
+		'[攻击:皮肤:西部魔影 锤石]':
+			{ target: '${C.path.project.autogen}reso/icon/Thresh/6.png' },
+		'[击杀:皮肤:西部魔影 锤石]':
+			{ target: '${C.path.project.autogen}reso/icon/Thresh/6.png' },
+		'[初遇:皮肤:西部魔影 厄加特]':
+			{ target: '${C.path.project.autogen}reso/icon/Urgot/9.png' },
+		'[击杀:皮肤:西部魔影 厄加特]':
+			{ target: '${C.path.project.autogen}reso/icon/Urgot/9.png' },
+		'[初遇:皮肤:西部牛仔 亚索]':
+			{ target: '${C.path.project.autogen}reso/icon/Yasuo/1.png' },
+		'[击杀:皮肤:西部牛仔 亚索]':
+			{ target: '${C.path.project.autogen}reso/icon/Yasuo/1.png' },
+
+
+		'[使用:Q技能:射手的专注]':
+			{ skill: '${C.path.project.autogen}reso/icon/Ashe/Q.png' },
+		'[使用:E技能:鹰击长空]':
+			{ skill: '${C.path.project.autogen}reso/icon/Ashe/E.png' },
+		'[使用:R技能:魔法水晶箭]':
+			{ skill: '${C.path.project.autogen}reso/icon/Ashe/R.png' },
+	},
+	lines: {
+		'D692D737': {
+			mark: '“Virtue”是西部天使艾希的战马，直译为“美德”，雌性（原文“her”）。由于没找到官方的音译用字，姑且译为“弗秋”',
+			afters: [
+				{
+					side: 'left',
+					head: '${C.path.project.autogen}reso/icon/Lucian/8.png',
+					line: 'ψ(`皿′)ψ',
+					hideEvent: true,
+				},
+			]
+		},
+		'E10DDD36': { mark: '“Jeremiah James”是厄加特在西部皮肤宇宙中的真名。\n语音中翻译为“杰里米亚”，皮肤传记中翻译为“耶利米”，都是常用的译名' },
+		'C2E4E872': { mark: '“Jeremiah James”是厄加特在西部皮肤宇宙中的真名。\n语音中翻译为“杰里米亚”，皮肤传记中翻译为“耶利米”，都是常用的译名' },
+	}
+};
