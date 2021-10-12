ff3e948a4525ef3a41636f44eb9fc2da23d688d4
Nuogz <zheung@vip.qq.com>
2021-10-12 16:01:32 +0800

    conf Dragon Yasuo

---
+++ reso/conf/157054.js
@@ -0,0 +1,67 @@
+export default {
+	title: '神龙尊者 昼日真龙 亚索',
+	champion: {
+		id: 157,
+		name: 'Yasuo',
+	},
+	skin: {
+		id: 54,
+		name: 'Truth Dragon'
+	},
+	emote: '๐•ᴗ•๐',
+	events: {
+		'[初遇:英雄:永恩]':
+			{ target: '${C.dirAutogen}/reso/icon/Yone/0.png' },
+		'[击杀:英雄:永恩]':
+			{ target: '${C.dirAutogen}/reso/icon/Yone/0.png' },
+		'[初遇:皮肤:神龙尊者 魔龙布兰德]':
+			{ target: '${C.dirAutogen}/reso/icon/Brand/8.png' },
+		'[击杀:皮肤:神龙尊者 魔龙布兰德]':
+			{ target: '${C.dirAutogen}/reso/icon/Brand/8.png' },
+		'[初遇:皮肤:神龙尊者 圣龙李青]':
+			{ target: '${C.dirAutogen}/reso/icon/LeeSin/31.png' },
+		'[击杀:皮肤:神龙尊者 圣龙李青]':
+			{ target: '${C.dirAutogen}/reso/icon/LeeSin/31.png' },
+		'[初遇:皮肤:神龙尊者 玉境灵龙 卡莎]':
+			{ target: '${C.dirAutogen}/reso/icon/Kaisa/29.png' },
+		'[击杀:皮肤:神龙尊者 玉境灵龙 卡莎]':
+			{ target: '${C.dirAutogen}/reso/icon/Kaisa/29.png' },
+		'[初遇:皮肤:神龙尊者 宁静之龙 卡尔玛]':
+			{ target: '${C.dirAutogen}/reso/icon/Karma/0.png' },
+		'[击杀:皮肤:神龙尊者 宁静之龙 卡尔玛]':
+			{ target: '${C.dirAutogen}/reso/icon/Karma/0.png' },
+		'[初遇:皮肤:神龙尊者 均衡之龙 沃利贝尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Volibear/7.png' },
+		'[击杀:皮肤:神龙尊者 均衡之龙 沃利贝尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Volibear/7.png' },
+		'[初遇:皮肤:神龙尊者 苍龙艾希]':
+			{ target: '${C.dirAutogen}/reso/icon/Ashe/23.png' },
+		'[击杀:皮肤:神龙尊者 苍龙艾希]':
+			{ target: '${C.dirAutogen}/reso/icon/Ashe/23.png' },
+		'[初遇:皮肤:神龙尊者 炎龙瑟提]':
+			{ target: '${C.dirAutogen}/reso/icon/Sett/8.png' },
+		'[击杀:皮肤:神龙尊者 炎龙瑟提]':
+			{ target: '${C.dirAutogen}/reso/icon/Sett/8.png' },
+
+		'[使用:Q技能:斩钢闪]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/Q1.png' },
+		'[使用:Q技能:斩钢闪:积攒1层旋风烈斩]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/Q2.png' },
+		'[使用:Q技能:斩钢闪:积攒2层旋风烈斩]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/Q3.png' },
+		'[使用:W技能:风之障壁]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/W.png' },
+		'[使用:E技能:踏前斩]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/E.png' },
+		'[使用:R技能:狂风绝息斩]':
+			{ skill: '${C.dirAutogen}/reso/icon/Yasuo/R.png' },
+	},
+	lines: {
+		'E39F7278': { mark: '对布兰德语音：与梦龙皮肤不相同\\n但在英文语音中并没有梦龙版本的语音\\。推测中文语音录音时，使用了略旧的台词本' },
+		'A4B57836': { mark: '对布兰德语音：与梦龙皮肤不相同\\n但在英文语音中并没有梦龙版本的语音\\。推测中文语音录音时，使用了略旧的台词本' },
+		'54D24952': { mark: '初次移动语音：只有前5句与梦龙皮肤不相同' },
+		'0BDF2C20': { mark: '移动语音：只有前5句与梦龙皮肤不相同' },
+		'3D129714': { mark: '长途移动语音：只有前5句与梦龙皮肤不相同' },
+		'0E5DFB18': { mark: '攻击语音：完全相同\\n但在英文语音中只有5句与梦龙皮肤不相同的\\。推测中文语音录音时，使用了略旧的台词本' },
+	}
+};
