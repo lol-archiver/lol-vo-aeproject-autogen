53431cc30ba7ca7d167636a00ecfbc82aeddf068
Nuogz <zheung@vip.qq.com>
2021-11-09 11:07:58 +0800

    051000

---
+++ reso/conf/051000.js
@@ -0,0 +1,88 @@
+export default {
+	title: '皮城女警 凯特琳',
+	champion: {
+		id: 51,
+		name: 'Caitlyn',
+	},
+	skin: {
+		id: 0,
+		name: '_Base'
+	},
+	emote: 'Ծ‸ Ծ ',
+	events: {
+		'[移动:初次:友方:英雄:金克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Jinx/0.png' },
+		'[移动:初次:友方:英雄:蔚]':
+			{ target: '${C.dirAutogen}/reso/icon/Vi/0.png' },
+		'[初遇:英雄:卡蜜尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Camille/0.png' },
+		'[击杀:英雄:卡蜜尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Camille/0.png' },
+		'[初遇:英雄:艾克]':
+			{ target: '${C.dirAutogen}/reso/icon/Ekko/0.png' },
+		'[击杀:英雄:艾克]':
+			{ target: '${C.dirAutogen}/reso/icon/Ekko/0.png' },
+		'[初遇:英雄:伊泽瑞尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Ezreal/0.png' },
+		'[击杀:英雄:伊泽瑞尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Ezreal/0.png' },
+		'[初遇:英雄:黑默丁格]':
+			{ target: '${C.dirAutogen}/reso/icon/Heimerdinger/0.png' },
+		'[击杀:英雄:黑默丁格]':
+			{ target: '${C.dirAutogen}/reso/icon/Heimerdinger/0.png' },
+		'[初遇:英雄:杰斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Jayce/0.png' },
+		'[击杀:英雄:杰斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Jayce/0.png' },
+		'[初遇:英雄:金克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Jinx/0.png' },
+		'[击杀:英雄:金克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Jinx/0.png' },
+		'[初遇:英雄:萨勒芬妮]':
+			{ target: '${C.dirAutogen}/reso/icon/Seraphine/0.png' },
+		'[击杀:英雄:萨勒芬妮]':
+			{ target: '${C.dirAutogen}/reso/icon/Seraphine/0.png' },
+		'[初遇:英雄:厄加特]':
+			{ target: '${C.dirAutogen}/reso/icon/Urgot/0.png' },
+		'[击杀:英雄:厄加特]':
+			{ target: '${C.dirAutogen}/reso/icon/Urgot/0.png' },
+		'[初遇:英雄:蔚]':
+			{ target: '${C.dirAutogen}/reso/icon/Vi/0.png' },
+		'[击杀:英雄:蔚]':
+			{ target: '${C.dirAutogen}/reso/icon/Vi/0.png' },
+		'[初遇:英雄:维克托]':
+			{ target: '${C.dirAutogen}/reso/icon/Viktor/0.png' },
+		'[击杀:英雄:维克托]':
+			{ target: '${C.dirAutogen}/reso/icon/Viktor/0.png' },
+		'[初遇:英雄:扎克]':
+			{ target: '${C.dirAutogen}/reso/icon/Zac/0.png' },
+		'[击杀:英雄:扎克]':
+			{ target: '${C.dirAutogen}/reso/icon/Zac/0.png' },
+
+		'[触发:P被动:爆头]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/P.png' },
+		'[使用:Q技能:和平使者]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/Q.png' },
+		'[使用:W技能:约德尔诱捕器]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/W.png' },
+		'[使用:W技能:约德尔诱捕器:禁锢]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/W.png' },
+		'[使用:E技能:90口径绳网]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/E.png' },
+		'[使用:E技能:90口径绳网:命中]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/E.png' },
+		'[使用:R技能:让子弹飞]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/R.png' },
+		'[使用:R技能:让子弹飞:命中]':
+			{ skill: '${C.dirAutogen}/reso/icon/Caitlyn/R.png' },
+
+	},
+	lines: {
+		'E39F7278': { mark: '对布兰德语音：与真龙皮肤不相同\\n但在英文语音中并没有梦龙版本的语音\\。推测中文语音录音时，使用了略旧的台词本' },
+		'A4B57836': { mark: '对布兰德语音：与真龙皮肤不相同\\n但在英文语音中并没有梦龙版本的语音\\。推测中文语音录音时，使用了略旧的台词本' },
+		'54D24952': { mark: '初次移动语音：只有前5句与真龙皮肤不相同' },
+		'0BDF2C20': { mark: '移动语音：只有前5句与真龙皮肤不相同' },
+		'3D129714': { mark: '长途移动语音：只有前5句与真龙皮肤不相同' },
+		'0E5DFB18': { mark: '攻击语音：完全相同\\n但在英文语音中只有5句与真龙皮肤不相同的\\。推测中文语音录音时，使用了略旧的台词本' },
+	}
+};
