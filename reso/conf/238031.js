2eeeac93d9486632e1f70a996364ee4eccd7e1af
DanoR <zheung@vip.qq.com>
2021-11-23 02:58:23 +0800

    update lib parseLine update resource add config Debonair Zed

---
+++ reso/conf/238031.js
@@ -0,0 +1,53 @@
+export default {
+	title: '影执事 劫',
+	champion: {
+		id: 238,
+		name: 'Zed',
+	},
+	skin: {
+		id: 31,
+		name: 'Debonair'
+	},
+	emote: '(ノಠ益ಠ)ノ',
+	events: {
+		'[初遇:英雄:凯隐]':
+			{ target: '${C.dirAutogen}/reso/icon/Kayn/0.png' },
+		'[初遇:英雄:慎]':
+			{ target: '${C.dirAutogen}/reso/icon/Shen/0.png' },
+		'[初遇:皮肤:焰执事 布兰德]':
+			{ target: '${C.dirAutogen}/reso/icon/Brand/21.png' },
+		'[击杀:皮肤:焰执事 布兰德]':
+			{ target: '${C.dirAutogen}/reso/icon/Brand/21.png' },
+		'[初遇:皮肤:刃执事 德莱文]':
+			{ target: '${C.dirAutogen}/reso/icon/Draven/29.png' },
+		'[击杀:皮肤:刃执事 德莱文]':
+			{ target: '${C.dirAutogen}/reso/icon/Draven/29.png' },
+		'[初遇:皮肤:幻执事 乐芙兰]':
+			{ target: '${C.dirAutogen}/reso/icon/LeBlanc/35.png' },
+		'[击杀:皮肤:幻执事 乐芙兰]':
+			{ target: '${C.dirAutogen}/reso/icon/LeBlanc/35.png' },
+		'[初遇:皮肤:烈阳执事 蕾欧娜]':
+			{ target: '${C.dirAutogen}/reso/icon/Leona/33.png' },
+		'[击杀:皮肤:烈阳执事 蕾欧娜]':
+			{ target: '${C.dirAutogen}/reso/icon/Leona/33.png' },
+		'[初遇:皮肤:虚空执事 玛尔扎哈]':
+			{ target: '${C.dirAutogen}/reso/icon/Malzahar/28.png' },
+		'[击杀:皮肤:虚空执事 玛尔扎哈]':
+			{ target: '${C.dirAutogen}/reso/icon/Malzahar/28.png' },
+		'[初遇:皮肤:无极执事 易]':
+			{ target: '${C.dirAutogen}/reso/icon/MasterYi/42.png' },
+		'[击杀:皮肤:无极执事 易]':
+			{ target: '${C.dirAutogen}/reso/icon/MasterYi/42.png' },
+
+		'[使用:Q技能:影奥义！诸刃]':
+			{ skill: '${C.dirAutogen}/reso/icon/Zed/Q.png' },
+		'[使用:W技能:影奥义！分身:互换位置]':
+			{ skill: '${C.dirAutogen}/reso/icon/Zed/W_2.png' },
+		'[使用:E技能:影奥义！鬼斩]':
+			{ skill: '${C.dirAutogen}/reso/icon/Zed/E.png' },
+		'[使用:R技能:禁奥义！瞬狱影杀阵]':
+			{ skill: '${C.dirAutogen}/reso/icon/Zed/R.png' },
+	},
+	lines: {
+	}
+};
