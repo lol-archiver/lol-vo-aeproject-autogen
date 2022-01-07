42975e0dbca9855f022392572a577ff0d09ee336
DanoR <zheung@vip.qq.com>
2022-01-07 14:23:04 +0800

    add conf Porcelain Protector Ezreal

---
+++ reso/conf/081025.js
@@ -0,0 +1,45 @@
+export default {
+	title: '青花瓷 伊泽瑞尔',
+	champion: {
+		id: 81,
+		name: 'Ezreal',
+	},
+	skin: {
+		id: 25,
+		name: 'Porcelain Protector'
+	},
+	emote: 'ヽ(=^･ω･^=)丿',
+	head: 'D:/Project/@nuogz-app/lol-vo-aeproject-autogen/reso/other/icon/POR_Ezreal.png',
+	splash: 'D:/Desk/ezcut.jpg',
+	events: {
+		'[初遇:皮肤:青花瓷 拉克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Lux/30.png' },
+		'[击杀:皮肤:青花瓷 拉克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Lux/30.png' },
+		'[初遇:皮肤:青花瓷 丽桑卓]':
+			{ target: '${C.dirAutogen}/reso/icon/Lissandra/23.png' },
+		'[击杀:皮肤:青花瓷 丽桑卓]':
+			{ target: '${C.dirAutogen}/reso/icon/Lissandra/23.png' },
+		'[初遇:皮肤:青花瓷 阿木木]':
+			{ target: '${C.dirAutogen}/reso/other/icon/POR_Amumu.png' },
+		'[击杀:皮肤:青花瓷 阿木木]':
+			{ target: '${C.dirAutogen}/reso/other/icon/POR_Amumu.png' },
+		'[初遇:皮肤:青花瓷 千珏]':
+			{ target: '${C.dirAutogen}/reso/icon/Kindred/12.png' },
+		'[击杀:皮肤:青花瓷 千珏]':
+			{ target: '${C.dirAutogen}/reso/icon/Kindred/12.png' },
+
+		'[使用:Q技能:秘术射击]':
+			{ skill: '${C.dirAutogen}/reso/icon/Ezreal/Q.png' },
+		'[使用:Q技能:秘术射击:未命中]':
+			{ skill: '${C.dirAutogen}/reso/icon/Ezreal/Q.png' },
+		'[使用:W技能:精华跃动]':
+			{ skill: '${C.dirAutogen}/reso/icon/Ezreal/W.png' },
+		'[使用:E技能:奥术跃迁]':
+			{ skill: '${C.dirAutogen}/reso/icon/Ezreal/E.png' },
+		'[使用:R技能:精准弹幕]':
+			{ skill: '${C.dirAutogen}/reso/icon/Ezreal/R.png' },
+	},
+	lines: {
+	}
+};
