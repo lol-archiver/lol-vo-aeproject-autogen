c63f3d298152ac4d6066894e0379f8c730ccd1ac
Nuogz <zheung@vip.qq.com>
2022-01-26 18:06:35 +0800

    3

--- src-extend/part/addOpener2.js
+++ src-extend/part/addOpener2.js
@@ -22,12 +22,28 @@ var splashesOpener = [
 	dirSplash + '28024.jpg'
 ];
 
+var ease1 = new KeyframeEase(0, 75);
+var ease2 = new KeyframeEase(0, 100);
+var setEase = function(propertySpec, key1, key2, ease) {
+	var length = propertySpec.keyInTemporalEase(key1).length;
+	var eases = length == 2 ? [ease, ease] : (length == 3 ? [ease, ease, ease] : [ease]);
+
+	propertySpec.setTemporalEaseAtKey(key1, propertySpec.keyInTemporalEase(key1), eases);
+	propertySpec.setTemporalEaseAtKey(key2, eases, propertySpec.keyOutTemporalEase(key2));
+};
+
 P.addOpener2 = function addOpener2() {
-	var compOpener = T.ensureComp('Opener', 10, app.project);
+	D.opener = 10;
+
+	var compOpener = T.ensureComp('comp-opener', D.opener, T.dirComp);
 	var layerOpener = compOpener.layers;
-	var avLayerOpenerMain = T.compMain.layers.add(compOpener, 10);
-	avLayerOpenerMain.startTime = 1;
-	avLayerOpenerMain.stretch = 50;
+
+	var avLayerOpenerMain = T.compMain.layers.add(compOpener, D.opener);
+	avLayerOpenerMain.stretch = 80;
+
+	avLayerOpenerMain.scale.setValueAtTime(5, [100, 100]);
+	avLayerOpenerMain.scale.setValueAtTime(7, [300, 300]);
+	setEase(avLayerOpenerMain.scale, 1, 2, ease1);
 
 
 	var widthGrid = C.widthVideo / 3;
@@ -45,43 +61,11 @@ P.addOpener2 = function addOpener2() {
 	var hScaleGrid3 = heightGrid3 / heightSplash * 100;
 
 
-	// var splashOpener07 = layerOpener.add(F(splashesOpener[7], T.dirImage), D.full); splashOpener07.name = 'splash-opener-7';
-	// var splashOpener08 = layerOpener.add(F(splashesOpener[8], T.dirImage), D.full); splashOpener08.name = 'splash-opener-8';
-	// var splashOpener09 = layerOpener.add(F(splashesOpener[9], T.dirImage), D.full); splashOpener09.name = 'splash-opener-9';
-	// var splashOpener10 = layerOpener.add(F(splashesOpener[10], T.dirImage), D.full); splashOpener10.name = 'splash-opener-10';
-	// var splashOpener11 = layerOpener.add(F(splashesOpener[11], T.dirImage), D.full); splashOpener11.name = 'splash-opener-11';
-	// var splashOpener12 = layerOpener.add(F(splashesOpener[12], T.dirImage), D.full); splashOpener12.name = 'splash-opener-12';
-	// var splashOpener13 = layerOpener.add(F(splashesOpener[13], T.dirImage), D.full); splashOpener13.name = 'splash-opener-13';
-	// var splashOpener14 = layerOpener.add(F(splashesOpener[14], T.dirImage), D.full); splashOpener14.name = 'splash-opener-14';
-	// var splashOpener15 = layerOpener.add(F(splashesOpener[15], T.dirImage), D.full); splashOpener15.name = 'splash-opener-15';
-	// var splashOpener16 = layerOpener.add(F(splashesOpener[16], T.dirImage), D.full); splashOpener16.name = 'splash-opener-16';
-
-	// var splashOpener06 = layerOpener.add(F(splashesOpener[6], T.dirImage), D.full); splashOpener06.name = 'splash-opener-6';
-	// var splashOpener05 = layerOpener.add(F(splashesOpener[5], T.dirImage), D.full); splashOpener05.name = 'splash-opener-5';
-
-	// var splashOpener03 = layerOpener.add(F(splashesOpener[3], T.dirImage), D.full); splashOpener03.name = 'splash-opener-3';
-	// var splashOpener04 = layerOpener.add(F(splashesOpener[4], T.dirImage), D.full); splashOpener04.name = 'splash-opener-4';
-
-
-	// var splashOpener02 = layerOpener.add(F(splashesOpener[2], T.dirImage), D.full); splashOpener02.name = 'splash-opener-2';
-
-
-	var ease1 = new KeyframeEase(0, 75);
-	var ease2 = new KeyframeEase(0, 100);
-	var setEase = function(propertySpec, key1, key2, ease) {
-		var length = propertySpec.keyInTemporalEase(key1).length;
-		var eases = length == 2 ? [ease, ease] : (length == 3 ? [ease, ease, ease] : [ease]);
-
-		propertySpec.setTemporalEaseAtKey(key1, propertySpec.keyInTemporalEase(key1), eases);
-		propertySpec.setTemporalEaseAtKey(key2, eases, propertySpec.keyOutTemporalEase(key2));
-	};
-
-
 	// splash-opener-top-left
-	var compTL = T.ensureComp('comp-splash-opener-top-left', 10, T.dirComp, widthGrid, heightGrid2);
-	layerOpener.add(compTL, D.full).position.setValue([wGrid(1, 0.5), hGrid2(1, 0.5)]);
+	var compTL = T.ensureComp('subcomp-splash-opener-top-left', D.opener, T.dirComp, widthGrid, heightGrid2);
+	layerOpener.add(compTL, D.opener).position.setValue([wGrid(1, 0.5), hGrid2(1, 0.5)]);
 
-	var splashTL = compTL.layers.add(F(splashesOpener[1], T.dirImage), D.full); splashTL.name = 'splash-opener-top-left';
+	var splashTL = compTL.layers.add(F(splashesOpener[1], T.dirImage), D.opener); splashTL.name = 'splash-opener-top-left';
 	splashTL.scale.setValue([hScaleGrid2, hScaleGrid2]);
 
 	splashTL.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid2(2, 0.5)]);
@@ -89,10 +73,10 @@ P.addOpener2 = function addOpener2() {
 	setEase(splashTL.position, 1, 2, ease1);
 
 	// splash-opener-bottom-left
-	var compBL = T.ensureComp('comp-splash-opener-bottom-left', 10, T.dirComp, widthGrid, heightGrid2);
-	layerOpener.add(compBL, D.full).position.setValue([wGrid(1, 0.5), hGrid2(2, 0.5)]);
+	var compBL = T.ensureComp('subcomp-splash-opener-bottom-left', D.opener, T.dirComp, widthGrid, heightGrid2);
+	layerOpener.add(compBL, D.opener).position.setValue([wGrid(1, 0.5), hGrid2(2, 0.5)]);
 
-	var splashBL = compBL.layers.add(F(splashesOpener[2], T.dirImage), D.full); splashBL.name = 'splash-opener-bottom-left';
+	var splashBL = compBL.layers.add(F(splashesOpener[2], T.dirImage), D.opener); splashBL.name = 'splash-opener-bottom-left';
 	splashBL.scale.setValue([hScaleGrid2, hScaleGrid2]);
 
 	splashBL.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid2(0, 0.5)]);
@@ -101,10 +85,10 @@ P.addOpener2 = function addOpener2() {
 
 
 	// splash-opener-top-right
-	var compTR = T.ensureComp('comp-splash-opener-top-right', 10, T.dirComp, widthGrid, heightGrid2);
-	layerOpener.add(compTR, D.full).position.setValue([wGrid(3, 0.5), hGrid2(1, 0.5)]);
+	var compTR = T.ensureComp('subcomp-splash-opener-top-right', D.opener, T.dirComp, widthGrid, heightGrid2);
+	layerOpener.add(compTR, D.opener).position.setValue([wGrid(3, 0.5), hGrid2(1, 0.5)]);
 
-	var splashTR = compTR.layers.add(F(splashesOpener[3], T.dirImage), D.full); splashTR.name = 'splash-opener-top-right';
+	var splashTR = compTR.layers.add(F(splashesOpener[3], T.dirImage), D.opener); splashTR.name = 'splash-opener-top-right';
 	splashTR.scale.setValue([hScaleGrid2, hScaleGrid2]);
 
 	splashTR.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid2(2, 0.5)]);
@@ -112,10 +96,10 @@ P.addOpener2 = function addOpener2() {
 	setEase(splashTR.position, 1, 2, ease1);
 
 	// splash-opener-bottom-right
-	var compBR = T.ensureComp('comp-splash-opener-bottom-right', 10, T.dirComp, widthGrid, heightGrid2);
-	layerOpener.add(compBR, D.full).position.setValue([wGrid(3, 0.5), hGrid2(2, 0.5)]);
+	var compBR = T.ensureComp('subcomp-splash-opener-bottom-right', D.opener, T.dirComp, widthGrid, heightGrid2);
+	layerOpener.add(compBR, D.opener).position.setValue([wGrid(3, 0.5), hGrid2(2, 0.5)]);
 
-	var splashBR = compBR.layers.add(F(splashesOpener[4], T.dirImage), D.full); splashBR.name = 'splash-opener-bottom-right';
+	var splashBR = compBR.layers.add(F(splashesOpener[4], T.dirImage), D.opener); splashBR.name = 'splash-opener-bottom-right';
 	splashBR.scale.setValue([hScaleGrid2, hScaleGrid2]);
 
 	splashBR.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid2(0, 0.5)]);
@@ -124,13 +108,13 @@ P.addOpener2 = function addOpener2() {
 
 
 	// splash-opener-top-center
-	var compTC = T.ensureComp('comp-splash-opener-top-center', 10, T.dirComp, widthGrid, heightGrid3);
-	var avCompTC = layerOpener.add(compTC, D.full);
+	var compTC = T.ensureComp('subcomp-splash-opener-top-center', D.opener, T.dirComp, widthGrid, heightGrid3);
+	var avCompTC = layerOpener.add(compTC, D.opener);
 	avCompTC.position.setValueAtTime(0, [wGrid(2, 0.5), hGrid3(1.5, 0.5)]);
 	avCompTC.position.setValueAtTime(1, [wGrid(2, 0.5), hGrid3(1, 0.5)]);
 	setEase(avCompTC.position, 1, 2, ease1);
 
-	var splashTC = compTC.layers.add(F(splashesOpener[5], T.dirImage), D.full); splashTC.name = 'splash-opener-top-center';
+	var splashTC = compTC.layers.add(F(splashesOpener[5], T.dirImage), D.opener); splashTC.name = 'splash-opener-top-center';
 	splashTC.scale.setValue([wScaleGrid, hScaleGrid3]);
 
 	splashTC.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid3(2, 0.5)]);
@@ -139,13 +123,13 @@ P.addOpener2 = function addOpener2() {
 
 
 	// splash-opener-bottom-center
-	var compBC = T.ensureComp('comp-splash-opener-bottom-center', 10, T.dirComp, widthGrid, heightGrid3);
-	var avCompBC = layerOpener.add(compBC, D.full);
+	var compBC = T.ensureComp('subcomp-splash-opener-bottom-center', D.opener, T.dirComp, widthGrid, heightGrid3);
+	var avCompBC = layerOpener.add(compBC, D.opener);
 	avCompBC.position.setValueAtTime(0, [wGrid(2, 0.5), hGrid3(2.5, 0.5)]);
 	avCompBC.position.setValueAtTime(1, [wGrid(2, 0.5), hGrid3(3, 0.5)]);
 	setEase(avCompBC.position, 1, 2, ease1);
 
-	var splashBC = compBC.layers.add(F(splashesOpener[6], T.dirImage), D.full); splashBC.name = 'splash-opener-bottom-center';
+	var splashBC = compBC.layers.add(F(splashesOpener[6], T.dirImage), D.opener); splashBC.name = 'splash-opener-bottom-center';
 	splashBC.scale.setValue([wScaleGrid, hScaleGrid3]);
 
 	splashBC.position.setValueAtTime(0, [wGrid(1, 0.5), hGrid3(0, 0.5)]);
@@ -154,69 +138,66 @@ P.addOpener2 = function addOpener2() {
 
 
 	// splash-opener-middle-center
-	var compMC = T.ensureComp('comp-splash-opener-middle-center', 10, T.dirComp, widthGrid, heightGrid3);
-	var avCompMC = layerOpener.add(compMC, D.full);
+	var compMC = T.ensureComp('subcomp-splash-opener-middle-center', D.opener, T.dirComp, widthGrid, heightGrid3);
+	var avCompMC = layerOpener.add(compMC, D.opener);
 	avCompMC.position.setValue([wGrid(2, 0.5), hGrid3(2, 0.5)]);
 
 	avCompMC.scale.setValueAtTime(0, [100, 0]);
 	avCompMC.scale.setValueAtTime(1, [100, 100]);
-	avCompMC.scale.setValueAtTime(3.5, [300, 300]);
 	setEase(avCompMC.scale, 1, 2, ease1);
-	setEase(avCompMC.scale, 2, 3, ease2);
 
-	var splashMC = compMC.layers.add(F(splashesOpener[0], T.dirImage), D.full); splashMC.name = 'splash-opener-middle-center';
+	var splashMC = compMC.layers.add(F(splashesOpener[0], T.dirImage), D.opener); splashMC.name = 'splash-opener-middle-center';
 	splashMC.scale.setValue([wScaleGrid, hScaleGrid3]);
 
-	// Logo Mask
-	var maskMC = compMC.layers.addSolid(T.rgb(45, 45, 45), 'mask-opener-middle-center', widthGrid, heightGrid3, 1);
+
+	// mask-opener
+	var maskMC = layerOpener.addSolid(T.rgb(45, 45, 45), 'mask-opener', C.widthVideo, C.heightVideo, 1);
+	maskMC.startTime = 1;
+	maskMC.outPoint = 5;
 	maskMC.adjustmentLayer = true;
 
-	maskMC.opacity.setValueAtTime(3.5, 100);
-	maskMC.opacity.setValueAtTime(4, 0);
+	maskMC.opacity.setValueAtTime(1, 0);
+	maskMC.opacity.setValueAtTime(2, 100);
 	setEase(maskMC.opacity, 1, 2, ease1);
+	maskMC.opacity.setValueAtTime(4, 100);
+	maskMC.opacity.setValueAtTime(5, 0);
+	setEase(maskMC.opacity, 3, 4, ease1);
 
 	var blurLogoMask = maskMC.effect.addProperty('ADBE Box Blur2');
 	blurLogoMask.blurRadius.setValue(14);
 
 
-	// Logo
-	var logo = layerOpener.add(F(dirSplash + 'logo.png', T.dirImage), 6); logo.name = 'logo';
-	logo.startTime = 1;
-	logo.scale.setValue([50, 50]);
+	// logo-font
+	var logoFont = layerOpener.add(F(dirSplash + 'logo.png', T.dirImage), 6); logoFont.name = 'logo-font';
+	logoFont.startTime = 1;
+	logoFont.outPoint = 5;
+	logoFont.scale.setValue([50, 50]);
 
-	logo.opacity.setValueAtTime(1, 0);
-	logo.opacity.setValueAtTime(2, 100);
-	setEase(logo.opacity, 1, 2, ease2);
-	logo.opacity.setValueAtTime(4, 0);
-	setEase(logo.opacity, 2, 3, ease1);
-
-
-	// Me
-	var meStatic = layerOpener.add(F(dirSplash + '4n.png', T.dirImage), 6); meStatic.name = 'me-static';
-	meStatic.startTime = 1;
-	meStatic.scale.setValue([40, 40]);
-	meStatic.position.setValue([720, 540]);
-
-	meStatic.opacity.setValueAtTime(1, 0);
-	meStatic.opacity.setValueAtTime(2, 100);
-	meStatic.opacity.setValueAtTime(2 + 0.0001, 0);
-	meStatic.opacity.setValueAtTime(3.5 - 0.0001, 0);
-	meStatic.opacity.setValueAtTime(3.5, 100);
-	setEase(meStatic.opacity, 1, 2, ease2);
-	meStatic.opacity.setValueAtTime(3.6, 0);
-	setEase(meStatic.opacity, 5, 6, ease1);
+	logoFont.opacity.setValueAtTime(1, 0);
+	logoFont.opacity.setValueAtTime(2, 100);
+	setEase(logoFont.opacity, 1, 2, ease2);
+	logoFont.opacity.setValueAtTime(4, 100);
+	logoFont.opacity.setValueAtTime(5, 0);
+	setEase(logoFont.opacity, 3, 4, ease1);
 
 
-	var meJugong = layerOpener.add(F(dirSplash + 'jugong480.gif', T.dirImage), 6); meJugong.name = 'me-jugong';
-	meJugong.startTime = 2;
-	meJugong.scale.setValue([40, 40]);
-	meJugong.position.setValue([720, 540]);
+	// logo-image
+	var logoImage = layerOpener.add(F(dirSplash + 'jugong480-500.mp4', T.dirImage), 6); logoImage.name = 'logo-image';
+	logoImage.startTime = 1;
+	logoImage.outPoint = 5;
+	logoImage.scale.setValue([40, 40]);
+	logoImage.position.setValue([720, 540]);
 
+	logoImage.opacity.setValueAtTime(1, 0);
+	logoImage.opacity.setValueAtTime(2, 100);
+	setEase(logoImage.opacity, 1, 2, ease2);
+	logoImage.opacity.setValueAtTime(4, 100);
+	logoImage.opacity.setValueAtTime(5, 0);
+	setEase(logoImage.opacity, 3, 4, ease1);
 
-
-
-
-	compOpener.openInViewer();
+	var colorKeyLogoImage = logoImage.effect.addProperty('ADBE Color Key');
+	colorKeyLogoImage.keyColor.setValue(T.rgb(255, 255, 255));
+	colorKeyLogoImage.colorTolerance.setValue(64);
 
 
 	return compOpener;
