efc97b0fb843a957d3ff3079ba608e0ddc388f33
DanoR <zheung@vip.qq.com>
2022-02-05 04:40:33 +0800

    Renata

---
+++ reso/conf/888000.js
@@ -0,0 +1,66 @@
+export default {
+	title: '炼金男爵 烈娜塔·戈拉斯克',
+	champion: {
+		id: 888,
+		name: 'Renata',
+	},
+	skin: {
+		id: 0,
+		name: '_Base'
+	},
+	emote: 'ヽ(=^･ω･^=)丿',
+	events: {
+		'[初遇:英雄:卡蜜尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Camille/0.png' },
+		'[初遇:英雄:艾克]':
+			{ target: '${C.dirAutogen}/reso/icon/Ekko/0.png' },
+		'[初遇:英雄:辛吉德]':
+			{ target: '${C.dirAutogen}/reso/icon/Singed/0.png' },
+		'[初遇:英雄:维克托]':
+			{ target: '${C.dirAutogen}/reso/icon/Viktor/0.png' },
+		'[初遇:英雄:莎弥拉]':
+			{ target: '${C.dirAutogen}/reso/icon/Samira/0.png' },
+		'[初遇:英雄:斯维因]':
+			{ target: '${C.dirAutogen}/reso/icon/Swain/0.png' },
+		'[初遇:英雄:金克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/Jinx/0.png' },
+		'[初遇:英雄:蔚]':
+			{ target: '${C.dirAutogen}/reso/icon/Vi/0.png' },
+		'[初遇:英雄:泽丽]':
+			{ target: '${C.dirAutogen}/reso/icon/Zeri/0.png' },
+		'[初遇:英雄:迦娜]':
+			{ target: '${C.dirAutogen}/reso/icon/Janna/0.png' },
+		'[初遇:英雄:萨勒芬妮]':
+			{ target: '${C.dirAutogen}/reso/icon/Seraphine/0.png' },
+
+
+		'[使用:Q技能:铁腕竞合]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/Q.png' },
+		'[使用:Q技能:铁腕竞合:再次施放]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/Q.png' },
+		'[使用:W技能:及时救难]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/W.png' },
+		'[使用:W技能:及时救难:救活队友]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/W.png' },
+		'[使用:E技能:忠诚激励]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/E.png' },
+		'[升级:R技能:恶意收购:初次]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/R.png' },
+		'[使用:R技能:恶意收购]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/R.png' },
+		'[使用:R技能:恶意收购:击杀敌方]':
+			{ target: '${C.dirAutogen}/reso/icon/Renata/R.png' },
+
+		'[使用:道具:监视图腾]':
+			{ skill: '${C.dirAutogen}/reso/icon/Item/3350_greateryellowtrinket.png' },
+
+		'[击杀:地区:祖安]':
+			{ target: '${C.dirAutogen}/reso/region/zaun.png' },
+		'[初遇:地区:皮尔特沃夫]':
+			{ target: '${C.dirAutogen}/reso/region/piltover.png' },
+		'[击杀:地区:皮尔吉沃特]':
+			{ target: '${C.dirAutogen}/reso/region/piltover.png' },
+	},
+	lines: {
+	}
+};
