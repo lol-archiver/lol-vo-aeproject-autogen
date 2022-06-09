8c698badc0fdfa40053d0d6543679e124eb1451b
Nuogz <zheung@vip.qq.com>
2022-06-09 11:35:31 +0800

    sp05

---
+++ reso/conf/sp05-project-ashe-forgotten.js
@@ -0,0 +1,31 @@
+export default {
+	title: '源计划：联合 艾希 被遗忘六年的嘲讽语音',
+	title1: '源计划：联合 艾希',
+	title2: '被遗忘六年的嘲讽语音',
+	splash: '${C.dirSplashes}/22/22008.jpg',
+	color: '#1FAAF1',
+	head: '${C.dirAutogen}/reso/icon/Ashe/8.png',
+	linesSpecial: [
+		{
+			event: '[嘲讽:友方:皮肤:源计划：风 亚索]',
+			line: '他们带走了你的兄弟，但他已经无法被拯救了',
+			mark: '这句语音是发布皮肤时就存在的',
+			file: '${C.dirAutogen}/reso/special/project-ashe-forgotten/TauntAlly3DYasuoSkin02[53128E2B][B5F3B3CF].wav',
+			target: '${C.dirAutogen}/reso/icon/Yasuo/2.png',
+		},
+		{
+			event: '[嘲讽:友方:皮肤:源计划：风 亚索]',
+			line: '请记住，你现在我们团队的一部分',
+			mark: '这句语音是2022年6月初才添加到皮肤中的\\n语音并不是新配的，而是被遗忘了，差不多6年',
+			file: '${C.dirAutogen}/reso/special/project-ashe-forgotten/TauntAlly3DYasuoSkin02[795FEE3C][4D6A1123].wav',
+			target: '${C.dirAutogen}/reso/icon/Yasuo/2.png',
+		},
+		{
+			event: '[嘲讽:友方:皮肤:源计划：风 亚索]',
+			line: '放开往日的负担，为我们的未来而战吧',
+			mark: '这句语音是2022年6月初才添加到皮肤中的\\n语音并不是新配的，而是被遗忘了，差不多6年',
+			file: '${C.dirAutogen}/reso/special/project-ashe-forgotten/TauntAlly3DYasuoSkin02[D2A68129][15A15F2A].wav',
+			target: '${C.dirAutogen}/reso/icon/Yasuo/2.png',
+		},
+	],
+};
