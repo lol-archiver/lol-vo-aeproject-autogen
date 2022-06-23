4c085d8fb44d557a58802604fde605f52015ba35
Nuogz <zheung@vip.qq.com>
2022-06-23 13:58:21 +0800

    nilah

---
+++ reso/conf/895000.js
@@ -0,0 +1,84 @@
+export default {
+	title: '不羁的欢乐 尼菈',
+	title1: '不羁的欢乐（猜的）',
+	title2: '尼菈',
+	champion: {
+		id: 895,
+		name: 'Nilah',
+	},
+	skin: {
+		id: 0,
+		name: '_Base'
+	},
+	color: '#1faaf1',
+	splashMiddle: '${C.dirAutogen}/reso/splash-left/895000.png',
+	// shade: {
+	// 	file: '${C.dirAutogen}/reso/shade/895000.png',
+	// 	scale: [150, 150],
+	// 	opacity: 45,
+	// 	// invert: false,
+	// },
+	events: {
+		'[攻击:生物:纳什男爵]':
+			{ target: '${C.dirAutogen}/reso/other/icon/baron.png' },
+		'[攻击:生物:峡谷先锋]':
+			{ target: '${C.dirAutogen}/reso/other/icon/rift-herald.png' },
+		'[攻击:生物:元素亚龙]':
+			{ target: '${C.dirAutogen}/reso/other/icon/fire-dragon.png' },
+		'[攻击:生物:远古巨龙]':
+			{ target: '${C.dirAutogen}/reso/other/icon/elder-dragon.png' },
+
+		'[初遇:英雄:费德提克]':
+			{ target: '${C.dirAutogen}/reso/icon/Fiddlesticks/0.png' },
+		'[击杀:英雄:费德提克]':
+			{ target: '${C.dirAutogen}/reso/icon/Fiddlesticks/0.png' },
+		'[初遇:英雄:千珏]':
+			{ target: '${C.dirAutogen}/reso/icon/Kindred/0.png' },
+		'[击杀:英雄:千珏]':
+			{ target: '${C.dirAutogen}/reso/icon/Kindred/0.png' },
+		'[初遇:英雄:佛耶戈]':
+			{ target: '${C.dirAutogen}/reso/icon/Viego/0.png' },
+		'[击杀:英雄:佛耶戈]':
+			{ target: '${C.dirAutogen}/reso/icon/Viego/0.png' },
+		'[初遇:英雄:格雷福斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Graves/0.png' },
+		'[击杀:英雄:格雷福斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Graves/0.png' },
+		'[初遇:英雄:崔斯特]':
+			{ target: '${C.dirAutogen}/reso/icon/TwistedFate/0.png' },
+		'[击杀:英雄:崔斯特]':
+			{ target: '${C.dirAutogen}/reso/icon/TwistedFate/0.png' },
+		'[初遇:英雄:潘森]':
+			{ target: '${C.dirAutogen}/reso/icon/Pantheon/0.png' },
+		'[初遇:英雄:卡莉丝塔]':
+			{ target: '${C.dirAutogen}/reso/icon/Kalista/0.png' },
+		'[初遇:英雄:赛娜]':
+			{ target: '${C.dirAutogen}/reso/icon/Senna/0.png' },
+		'[初遇:英雄:莫德凯撒]':
+			{ target: '${C.dirAutogen}/reso/icon/Mordekaiser/0.png' },
+		'[初遇:英雄:斯维因]':
+			{ target: '${C.dirAutogen}/reso/icon/Swain/0.png' },
+		'[初遇:英雄:俄洛伊]':
+			{ target: '${C.dirAutogen}/reso/icon/Illaoi/0.png' },
+		'[初遇:英雄:卡尔玛]':
+			{ target: '${C.dirAutogen}/reso/icon/Karma/0.png' },
+		'[初遇:英雄:沃利贝尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Volibear/0.png' },
+
+		'[普攻]、[使用:Q技能:游刃万变:强化攻击]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/Q.png' },
+		'[触发:P被动:酣庆无终:治疗或提供护盾]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/P.png' },
+		'[使用:Q技能:游刃万变]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/Q.png' },
+		'[使用:W技能:轻纱飞漾]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/W.png' },
+		'[使用:E技能:纵情逐流]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/E.png' },
+		'[使用:R技能:神恩激荡]':
+			{ skill: '${C.dirAutogen}/reso/icon/Nilah/R.png' },
+	},
+	lines: {
+		'6FFBB00E': { mark: '无法确定抢到了男爵和峡谷先锋能否触发语音', },
+	}
+};
