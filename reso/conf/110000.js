6028ec34a8e40623a5a5c9afeb8abc349d27c65c
Nuogz <zheung@vip.qq.com>
2023-06-28 10:37:34 +0800

    update parse-ilne mark; 110000 2023

---
+++ reso/conf/110000.js
@@ -0,0 +1,75 @@
+export default {
+	title: '惩戒之箭 韦鲁斯',
+	title1: '惩戒之箭',
+	title2: '韦鲁斯',
+	champion: {
+		id: 110,
+		name: 'Varus',
+	},
+	skin: {
+		id: 0,
+		name: '_Base'
+	},
+	color: '#855BB4',
+	splashMiddle: '${C.dirAutogen}/reso/splash-left/110000.png',
+	events: {
+		'[初遇:英雄:纳亚菲利]':
+			{ target: '${C.dirAutogen}/reso/icon/-1.png' },
+		'[击杀:英雄:纳亚菲利]':
+			{ target: '${C.dirAutogen}/reso/icon/-1.png' },
+		'[移动:初次:友方:英雄:纳亚菲利]':
+			{ target: '${C.dirAutogen}/reso/icon/-1.png' },
+		'[初遇:英雄:亚托克斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Aatrox/0.png' },
+		'[击杀:英雄:亚托克斯]':
+			{ target: '${C.dirAutogen}/reso/icon/Aatrox/0.png' },
+		'[初遇:英雄:凯隐]':
+			{ target: '${C.dirAutogen}/reso/icon/Kayn/0.png' },
+		'[击杀:英雄:凯隐]':
+			{ target: '${C.dirAutogen}/reso/icon/Kayn/0.png' },
+		'[初遇:英雄:艾希]':
+			{ target: '${C.dirAutogen}/reso/icon/Ashe/0.png' },
+		'[击杀:英雄:艾希]':
+			{ target: '${C.dirAutogen}/reso/icon/Ashe/0.png' },
+		'[初遇:英雄:卡莉丝塔]':
+			{ target: '${C.dirAutogen}/reso/icon/Kalista/0.png' },
+		'[击杀:英雄:卡莉丝塔]':
+			{ target: '${C.dirAutogen}/reso/icon/Kalista/0.png' },
+		'[初遇:英雄:希维尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Sivir/0.png' },
+		'[击杀:英雄:希维尔]':
+			{ target: '${C.dirAutogen}/reso/icon/Sivir/0.png' },
+		'[初遇:英雄:卡尔玛]':
+			{ target: '${C.dirAutogen}/reso/icon/Karma/0.png' },
+		'[击杀:英雄:卡尔玛]':
+			{ target: '${C.dirAutogen}/reso/icon/Karma/0.png' },
+
+		'[使用:Q穿刺之箭:蓄力时]':
+			{ skill: '${C.dirAutogen}/reso/icon/Varus/Q.png' },
+		'[使用:Q穿刺之箭:取消释放时]':
+			{ skill: '${C.dirAutogen}/reso/icon/Varus/Q.png' },
+		'[使用:E恶灵箭雨]':
+			{ skill: '${C.dirAutogen}/reso/icon/Varus/E.png' },
+		'[升级:R腐败锁链:初次]':
+			{ skill: '${C.dirAutogen}/reso/icon/Varus/R.png' },
+		'[使用:R腐败锁链]':
+			{ skill: '${C.dirAutogen}/reso/icon/Varus/R.png' },
+	},
+	lines: {
+		'6310121D': { mark: '“瓦尔茂”原文“Valmar”\\，被韦鲁斯融合身体的俩人之一\\。三人的身体被融合为一，灵魂均在身体内' },
+		'C14D695B': { mark: '“凯伊”原文“Kai”\\，被韦鲁斯融合身体的俩人之一\\。三人的身体被融合为一，灵魂均在身体内' },
+		'0097385E': { mark: '“瓦尔茂”原文“Valmar”，“凯伊”原文“Kai”\\\\，被韦鲁斯融合身体的俩人\\。三人的身体被融合为一，灵魂均在身体内' },
+		'20500F27': { mark: '“瓦尔茂”原文“Valmar”，“凯伊”原文“Kai”\\\\，被韦鲁斯融合身体的俩人\\。三人的身体被融合为一，灵魂均在身体内' },
+		'1A59F215': { mark: '“亚”在英文中对应是“aa”' },
+		'6550FA75': { mark: '希维尔的武器是“恰丽喀尔”\\，原本是恕瑞玛初代女皇瑟塔卡的武器' },
+		'268F8A41': { mark: '这里的“心”引申自“灵犀之侣”的原文\\，“heartlight”，特指瓦尔茂和凯伊的关系' },
+		'84322313': { mark: '“瓦里伊娃”原文“Valeeva”\\，“佐兰妮”原文“Xolaani”\\，均是暗裔' },
+		'2B76B37A': { mark: '“灵犀”引申自“灵犀之侣”，特指瓦尔茂和凯伊的关系' },
+		'474E7C6F': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+		'F986426B': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+		'2C6CB241': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+		'09E9ED5B': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+		'5208F16B': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+		'181A2718': { mark: '在SkinSpotlight视频中\\，表示需要通过Q技能击杀英雄触发\\。但实际解包后发现并不需要Q技能击杀' },
+	}
+};
