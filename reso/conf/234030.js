d68dc14743c74faae324fe19b4a74c1ae0587276
Nuogz <zheung@vip.qq.com>
2023-07-20 10:23:45 +0800

    improve format-event add config 234030 update resource

---
+++ reso/conf/234030.js
@@ -0,0 +1,93 @@
+export default {
+	title: '斗魂觉醒 佛耶戈',
+	title1: '斗魂觉醒',
+	title2: '佛耶戈',
+	champion: {
+		id: 234,
+		name: 'Viego',
+	},
+	skin: {
+		id: 30,
+		name: 'Soul Fighter'
+	},
+	color: '#D85639',
+	splashMiddle: '${C.dirAutogen}/reso/splash-left/234030.png',
+	head: '${C.dirAutogen}/reso/icon/@unofficial/sf-viego-2.png',
+	events: {
+		'[攻击:生物:纳什男爵]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/baron.png' },
+		'[击杀:生物:纳什男爵]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/baron.png' },
+		'[击杀:生物:龙]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/fire-dragon.png' },
+		'[击杀:生物:绯红印记树怪]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/red-brambleback.png' },
+		'[击杀:生物:苍蓝雕纹魔像]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/blue-sentinel.png' },
+		'[击杀:生物:野区普通生物]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/murkwolf.png', mark: '魔沼蛙/暗影狼/锋喙鸟/远古石甲虫，不包括峡谷迅捷蟹' },
+		'[击杀:建筑:防御塔]':
+			{ target: '${C.dirAutogen}/reso/icon/turret.png' },
+
+
+		'[移动:初次:友方:皮肤:斗魂觉醒 莎弥拉]':
+			{ target: '${C.dirAutogen}/reso/icon/samira/30.png' },
+		'[初遇:皮肤:斗魂觉醒 莎弥拉]':
+			{ target: '${C.dirAutogen}/reso/icon/samira/30.png' },
+		'[击杀:皮肤:斗魂觉醒 莎弥拉]':
+			{ target: '${C.dirAutogen}/reso/icon/samira/30.png' },
+		'[初遇:皮肤:斗魂觉醒 伊芙琳]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-evelynn-2.png' },
+		'[击杀:皮肤:斗魂觉醒 伊芙琳]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-evelynn-2.png' },
+		'[初遇:皮肤:斗魂觉醒 格温]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-gwen-2.png' },
+		'[击杀:皮肤:斗魂觉醒 格温]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-gwen-2.png' },
+		'[初遇:皮肤:斗魂觉醒 烬]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-jhin-2.png' },
+		'[击杀:皮肤:斗魂觉醒 烬]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-jhin-2.png' },
+		'[初遇:皮肤:斗魂觉醒 拉克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/lux/38.png' },
+		'[击杀:皮肤:斗魂觉醒 拉克丝]':
+			{ target: '${C.dirAutogen}/reso/icon/lux/38.png' },
+		'[初遇:皮肤:斗魂觉醒 纳亚菲利]':
+			{ target: '${C.dirAutogen}/reso/icon/naafiri/1.png' },
+		'[击杀:皮肤:斗魂觉醒 纳亚菲利]':
+			{ target: '${C.dirAutogen}/reso/icon/naafiri/1.png' },
+		'[初遇:皮肤:斗魂觉醒 派克]':
+			{ target: '${C.dirAutogen}/reso/icon/pyke/53.png' },
+		'[击杀:皮肤:斗魂觉醒 派克]':
+			{ target: '${C.dirAutogen}/reso/icon/pyke/53.png' },
+		'[初遇:皮肤:斗魂觉醒 瑟提]':
+			{ target: '${C.dirAutogen}/reso/icon/sett/45.png' },
+		'[击杀:皮肤:斗魂觉醒 瑟提]':
+			{ target: '${C.dirAutogen}/reso/icon/sett/45.png' },
+		'[初遇:皮肤:斗魂觉醒 萨科]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-shaco-2.png' },
+		'[击杀:皮肤:斗魂觉醒 萨科]':
+			{ target: '${C.dirAutogen}/reso/icon/@unofficial/sf-shaco-2.png' },
+
+
+		'[使用:Q破败王剑]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/Q.png' },
+		'[使用:W千载幽咽]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/W.png' },
+		'[使用:W千载幽咽:命中]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/W.png' },
+		'[使用:W千载幽咽:未命中]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/W.png' },
+		'[使用:E茫茫焦土]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/E.png' },
+		'[升级:R痛贯天灵:初次]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/R.png' },
+		'[使用:R痛贯天灵]':
+			{ skill: '${C.dirAutogen}/reso/icon/Viego/R.png' },
+	},
+	lines: {
+		'AC69DC4E': { mark: '引申自一些街机游戏中的爬塔设定，如《真人快打》、《罪恶装备》' },
+		'D2B56332': { mark: '引申自动画《龙珠超》中贝吉塔的台词\\：“只有废物才会放弃他的原则和尊严”' },
+		'3D49B26F': { mark: '原文“A challenger approaches”\\，引申自游戏《全明星大乱斗》中的挑战者模式' },
+	}
+};
