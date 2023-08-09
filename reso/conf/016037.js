b2471efea81bd69d1a018737219cffad29f37e2d
DanoR <zheung@vip.qq.com>
2023-08-09 09:58:35 +0800

    016037

---
+++ reso/conf/016037.js
@@ -0,0 +1,66 @@
+export default {
+	title: '玉剑传说 星仙子 索拉卡',
+	title1: '玉剑传说',
+	title2: '星仙子 索拉卡',
+	champion: {
+		id: 16,
+		name: 'Soraka',
+	},
+	skin: {
+		id: 37,
+		name: 'Immortal Journey'
+	},
+	color: '#F5AF35',
+	splashMiddle: '${C.dirAutogen}/reso/splash-left/016037.png',
+	audios: 'D:/project/@lol/lol-vo-extract/@final/[016037]玉剑传说 星仙子[Soraka@PBE1@zh_cn]',
+	events: {
+		'[攻击:生物:纳什男爵]':
+			{ target: '${C.dirAutogen}/reso/icon/@monster/baron.png' },
+		'[击杀:建筑:防御塔]':
+			{ target: '${C.dirAutogen}/reso/icon/turret.png' },
+		'[使用:道具:监视图腾]':
+			{ skill: '${C.dirAutogen}/reso/icon/Item/3350_greateryellowtrinket.png' },
+
+
+		'[初遇:皮肤:玉剑传说 琴仙子]':
+			{ target: '${C.dirAutogen}/reso/icon/sona/45.png' },
+		'[击杀:皮肤:玉剑传说 琴仙子]':
+			{ target: '${C.dirAutogen}/reso/icon/sona/45.png' },
+		'[初遇:皮肤:玉剑传说 舞龙卫]':
+			{ target: '${C.dirAutogen}/reso/icon/shyvana/17.png' },
+		'[击杀:皮肤:玉剑传说 舞龙卫]':
+			{ target: '${C.dirAutogen}/reso/icon/shyvana/17.png' },
+		'[初遇:皮肤:玉剑传说 无劫]':
+			{ target: '${C.dirAutogen}/reso/icon/zed/49.png' },
+		'[击杀:皮肤:玉剑传说 无劫]':
+			{ target: '${C.dirAutogen}/reso/icon/zed/49.png' },
+		'[初遇:皮肤:玉剑传说 翼剑仙]':
+			{ target: '${C.dirAutogen}/reso/icon/kayle/57.png' },
+		'[击杀:皮肤:玉剑传说 翼剑仙]':
+			{ target: '${C.dirAutogen}/reso/icon/kayle/57.png' },
+		'[初遇:皮肤:玉剑传说 神机花火]':
+			{ target: '${C.dirAutogen}/reso/icon/zeri/19.png' },
+		'[击杀:皮肤:玉剑传说 神机花火]':
+			{ target: '${C.dirAutogen}/reso/icon/zeri/19.png' },
+
+
+		'[触发:P拯救]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/P.png' },
+		'[使用:Q流星坠落]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/Q.png' },
+		'[使用:Q流星坠落:回复生命值时]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/Q.png' },
+		'[使用:W星之灌注]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/W.png' },
+		'[使用:W星之灌注:带有Q活力焕发时]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/W.png' },
+		'[使用:E星体结界:命中时]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/E.png' },
+		'[升级:R祈愿:初次]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/R.png' },
+		'[使用:R祈愿]':
+			{ skill: '${C.dirAutogen}/reso/icon/soraka/R.png' },
+	},
+	lines: {
+	}
+};
